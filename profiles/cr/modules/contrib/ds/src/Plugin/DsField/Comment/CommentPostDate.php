758ec915a5d34ec186100a607a20becf10259c32
Gustavo Liedke <g.liedke@comicrelief.com>
2016-02-25 12:24:21 +0000

    PLAT-44 add DS FG modules

---
+++ profiles/cr/modules/contrib/ds/src/Plugin/DsField/Comment/CommentPostDate.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\ds\Plugin\DsField\Comment\CommentPostDate.
+ */
+
+namespace Drupal\ds\Plugin\DsField\Comment;
+
+use Drupal\ds\Plugin\DsField\Date;
+
+/**
+ * Plugin that renders the post date of a comment.
+ *
+ * @DsField(
+ *   id = "comment_post_date",
+ *   title = @Translation("Post date"),
+ *   entity_type = "comment",
+ *   provider = "comment"
+ * )
+ */
+class CommentPostDate extends Date {
+
+  /**
+   * Returns the entity render key for this field.
+   */
+  public function getRenderKey() {
+    return 'created';
+  }
+
+}
