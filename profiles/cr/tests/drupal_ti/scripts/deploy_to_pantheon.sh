39447fde1fe6fc3e0dc8cfa15a007baef89333b5
Peter Vanhee <peter.vanhee@gmail.com>
2016-03-04 17:29:03 +0000

    PLAT-55 Refactoring

--- profiles/cr/tests/drupal_ti/scripts/deploy_to_pantheon.sh
+++ profiles/cr/tests/drupal_ti/scripts/deploy_to_pantheon.sh
@@ -7,15 +7,23 @@ terminus auth login "$PEMAIL" --password="$PPASS"
 # Change connection mode to Git
 terminus site set-connection-mode --site="$PUUID" --env="$PENV" --mode=git
 
-# - git config --global user.email "$_CI_BOT_EMAIL"
-# - git config --global user.name "$_CI_BOT_NAME"
-
 # We need to compile CSS
 # Then, commit all to the current branch after fixing .gitignore
+git config --global user.email "$CI_BOT_EMAIL"
+git config --global user.name "$CI_BOT_NAME"
 
+cd "$DRUPAL_TI_THEME_DIR"
+git add --force css
+git add --force js
+cd "$TRAVIS_BUILD_DIR"
+
+export CI_COMMIT_MSG="Branch $TRAVIS_BRANCH compiled CSS"
+git commit -a -m "Built by CI - $CI_COMMIT_MSG"
+
+# Add a new remote for Pantheon
 git remote add pantheon ssh://codeserver.$PENV.$PUUID@codeserver.$PENV.$PUUID.drush.in:2222/~/repository.git
 
-git log --decorate
+# And push all code
 git push pantheon HEAD:master --force
 
 # Change connection mode back to SFTP so we can install
