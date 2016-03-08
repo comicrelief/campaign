6ad8cb0758a3c5873e9b6b3b28491ab1ca0b8e1b
Saphyel <Saphyel@gmail.com>
2016-03-08 13:59:16 +0000

    PLAT-61 Test paths

--- profiles/cr/tests/drupal_ti/scripts/script.sh
+++ profiles/cr/tests/drupal_ti/scripts/script.sh
@@ -3,7 +3,7 @@
 # Behat integration - Script step.
 
 set -e $DRUPAL_TI_DEBUG
-
+pwd
 # Ensure we are in the right directory, we need to overwrite this here
 # since it is different from Drupal TI's default setup
 DRUPAL_TI_DRUPAL_DIR="$TRAVIS_BUILD_DIR"
@@ -12,12 +12,14 @@ DRUPAL_TI_DRUPAL_DIR="$TRAVIS_BUILD_DIR"
 # needed for example for the drush runner.
 cd "$DRUPAL_TI_BEHAT_DIR"
 
+# Check code quality
+phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
+phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme ../../modules/custom ../../themes/custom
+phpmd modules/custom text codesize,unusedcode,naming
+
 # We need to create a behat.yml file from behat.yml.dist.
 drupal_ti_replace_behat_vars
 
 # And run the tests.
 ARGS=( $DRUPAL_TI_BEHAT_ARGS )
 ./vendor/bin/behat "${ARGS[@]}"
-phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
-phpcs --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme ../../modules/custom ../../themes/custom
-phpmd modules/custom text codesize,unusedcode,naming
