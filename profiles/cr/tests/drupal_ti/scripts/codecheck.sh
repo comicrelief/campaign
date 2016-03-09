83ec47918b0ab6d42812b9c437c0455d2e03e078
Saphyel <Saphyel@gmail.com>
2016-03-09 10:28:23 +0000

    PLAT-61 Forcing strings

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -5,9 +5,9 @@
 cd "$DRUPAL_TI_BEHAT_DIR"
 cd "vendor"
 
-bin/phpcs --config-set installed_paths ${CS_PROFILE}
-bin/phpcs --standard=${CS_STAND} --extensions=${CS_EXT} ${CODE_BUILD_DIR} --runtime-set ignore_warnings_on_exit 1
-bin/phpmd ${CODE_BUILD_DIR} text codesize,unusedcode,naming
+bin/phpcs --config-set "installed_paths" ${CS_PROFILE}
+bin/phpcs --standard="${CS_STAND}" --extensions=${CS_EXT} ${CODE_BUILD_DIR} --runtime-set "ignore_warnings_on_exit 1"
+bin/phpmd ${CODE_BUILD_DIR} "text codesize,unusedcode,naming"
 bin/phpcpd ${CODE_BUILD_DIR}
 
 cd "$TRAVIS_BUILD_DIR"
