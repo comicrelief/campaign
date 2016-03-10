759dbeaaa01831e63c3fe935925258ea93974b76
Saphyel <Saphyel@gmail.com>
2016-03-10 10:07:24 +0000

    PLAT-61 Hardcode more paths

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # @file
-# Behat integration - Script step.
+# Code quality - Script step.
 
+cd "$TRAVIS_BUILD_DIR"
 CMDBIN="profiles/cr/tests/behat/vendor/bin"
+DRUPAL_CODER="profiles/cr/tests/behat/vendor/drupal/coder/coder_sniffer"
 
-"${CMDBIN}/phpcs" --config-set installed_paths "${CMDBIN}/../drupal/coder/coder_sniffer"
+"${CMDBIN}/phpcs" --config-set installed_paths "${DRUPAL_CODER}"
 "${CMDBIN}/phpcs" --standard=DrupalPractice --extensions=php,module,inc,install,test,profile,theme profiles/cr/modules/custom --runtime-set ignore_warnings_on_exit 1
 "${CMDBIN}/phpmd" profiles/cr/modules/custom text codesize,unusedcode,naming
 "${CMDBIN}/phpcpd" profiles/cr/modules/custom
