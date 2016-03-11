6cb13a27b5eb000d8c828a2286bea9adb671dded
Saphyel <Saphyel@gmail.com>
2016-03-11 15:49:58 +0000

    PLAT-61 Fix bug

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -5,6 +5,7 @@ VENDOR_PATH="profiles/cr/tests/behat/vendor"
 CODE_PATH="profiles/cr/modules/custom"
 STANDARD="--standard=DrupalPractice"
 EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
+echo $($DRUPAL_TI_BEHAT_DIR)
 
 $VENDOR_PATH/bin/phpcs --config-set installed_paths "$VENDOR_PATH/drupal/coder/coder_sniffer"
 $VENDOR_PATH/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
