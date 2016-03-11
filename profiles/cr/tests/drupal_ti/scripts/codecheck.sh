97ca501609ac7a439499b267b5eaf29b70df17c2
Saphyel <Saphyel@gmail.com>
2016-03-11 11:25:34 +0000

    PLAT-61 Add phpcs

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -3,5 +3,9 @@
 
 VENDOR_PATH="profiles/cr/tests/behat/vendor"
 CODE_PATH="profiles/cr/modules/custom"
+STANDARD="--standard=DrupalPractice"
+EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
 
 $VENDOR_PATH/bin/phpmd "$CODE_PATH" text codesize,unusedcode,naming
+$VENDOR_PATH/bin/phpcs --config-set installed_paths "$VENDOR_PATH/drupal/coder/coder_sniffer"
+$VENDOR_PATH/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
