09a6632230609d42603e276366bbfc0ec6dad93d
Saphyel <Saphyel@gmail.com>
2016-03-11 11:34:23 +0000

    PLAT-61 Add cpd

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -6,6 +6,7 @@ CODE_PATH="profiles/cr/modules/custom"
 STANDARD="--standard=DrupalPractice"
 EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
 
-$VENDOR_PATH/bin/phpmd "$CODE_PATH" text codesize,unusedcode,naming
 $VENDOR_PATH/bin/phpcs --config-set installed_paths "$VENDOR_PATH/drupal/coder/coder_sniffer"
 $VENDOR_PATH/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
+$VENDOR_PATH/bin/phpmd "$CODE_PATH" text codesize,unusedcode,naming
+$VENDOR_PATH/bin/phpcpd "$CODE_PATH"
