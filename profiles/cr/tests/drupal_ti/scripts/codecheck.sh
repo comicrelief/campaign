c68a6ce04913f30a2fd04a4517ef7b43f39c965f
Saphyel <Saphyel@gmail.com>
2016-03-14 11:02:05 +0000

    PLAT-79 fixing path

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -5,7 +5,11 @@ CODE_PATH="profiles/cr/modules/custom"
 STANDARD="--standard=DrupalPractice"
 EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
 
-$DRUPAL_TI_BEHAT_DIR/vendor/bin/phpcs --config-set installed_paths "vendor/drupal/coder/coder_sniffer"
-$DRUPAL_TI_BEHAT_DIR/vendor/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
-$DRUPAL_TI_BEHAT_DIR/vendor/bin/phpmd "$CODE_PATH" text codesize,unusedcode,naming
-$DRUPAL_TI_BEHAT_DIR/vendor/bin/phpcpd "$CODE_PATH"
+cd "$DRUPAL_TI_BEHAT_DIR"
+
+vendor/bin/phpcs --config-set installed_paths "vendor/drupal/coder/coder_sniffer"
+
+cd "$TRAVIS_BUILD_DIR"
+vendor/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
+vendor/bin/phpmd "$CODE_PATH" text codesize,unusedcode,naming
+vendor/bin/phpcpd "$CODE_PATH"
