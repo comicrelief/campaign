3089be5ed0ddf285bfa52effdaf37b4130ad201f
Saphyel <Saphyel@gmail.com>
2016-03-14 11:40:54 +0000

    PLAT-79 show path

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -5,10 +5,9 @@ CODE_PATH="profiles/cr/modules/custom"
 STANDARD="--standard=DrupalPractice"
 EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
 
-cd "$DRUPAL_TI_BEHAT_DIR"
-pwd
-ls -al "vendor/drupal/coder/coder_sniffer"
-vendor/bin/phpcs --config-set installed_paths "vendor/drupal/coder/coder_sniffer"
+cd "$DRUPAL_TI_BEHAT_DIR/vendor/bin"
+ls -al "$DRUPAL_TI_BEHAT_DIR/vendor/drupal/coder/coder_sniffer"
+phpcs --config-set installed_paths "$DRUPAL_TI_BEHAT_DIR/vendor/drupal/coder/coder_sniffer"
 
 cd "$TRAVIS_BUILD_DIR"
 $DRUPAL_TI_BEHAT_DIR/vendor/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
