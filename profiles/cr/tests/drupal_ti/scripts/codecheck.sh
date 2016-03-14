918514ad420b9d3a7a6cc76b16df13f138d13a5b
Saphyel <Saphyel@gmail.com>
2016-03-14 11:33:25 +0000

    PLAT-79 show path

--- profiles/cr/tests/drupal_ti/scripts/codecheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/codecheck.sh
@@ -6,7 +6,8 @@ STANDARD="--standard=DrupalPractice"
 EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
 
 cd "$DRUPAL_TI_BEHAT_DIR"
-
+pwd
+ls -al "vendor/drupal/coder/coder_sniffer"
 vendor/bin/phpcs --config-set installed_paths "vendor/drupal/coder/coder_sniffer"
 
 cd "$TRAVIS_BUILD_DIR"
