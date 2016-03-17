980c9c5457b7a18a15eb34fabac4926eedf64c48
Saphyel <Saphyel@gmail.com>
2016-03-17 13:02:14 +0000

    PLAT-29 Trying the contrib way, don't work

--- profiles/cr/modules/custom/cr_rabbitmq/src/EventSubscriber/RabbitmqSubscriber.php
+++ profiles/cr/modules/custom/cr_rabbitmq/src/EventSubscriber/RabbitmqSubscriber.php
@@ -6,8 +6,46 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Channel\AMQPChannel;
+use Drupal\rabbitmq\Queue\Queue;
+use Drupal\rabbitmq\Queue\QueueFactory;
 
 class RabbitmqSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The default queue, handled by Beanstalkd.
+   *
+   * @var \Drupal\beanstalkd\Queue\BeanstalkdQueue
+   */
+  protected $queue;
+  /**
+   * Server factory.
+   *
+   * @var \Drupal\rabbitmq\Connection
+   */
+  protected $connectionFactory;
+
+  /**
+   * Initialize a server and free channel.
+   *
+   * @return \AMQPChannel
+   *   A channel to the default queue.
+   */
+  protected function initChannel() {
+    // $this->connectionFactory = $container->get('rabbitmq.connection.factory');
+    $this->connectionFactory = \Drupal::service('rabbitmq.connection.factory');
+    $connection = $this->connectionFactory->getConnection();
+    $channel = $connection->channel();
+    $name = 'loca';
+    $passive = FALSE;
+    $durable = FALSE;
+    $exclusive = FALSE;
+    $auto_delete = FALSE;
+
+    $channel->queue_declare($name, $passive, $durable, $exclusive, $auto_delete);
+
+    return $channel;
+  }
 
   /**
    * Execute some rabbits.
@@ -33,12 +71,17 @@ class RabbitmqSubscriber implements EventSubscriberInterface {
     $channel->close();
     $connection->close();
   }
+  public function rabbitLauncher() {
+    $this->initChannel();
+    $this->queue->createItem('foo');
+  }
 
   /**
    * {@inheritdoc}
    */
   static function getSubscribedEvents() {
-    $events[KernelEvents::REQUEST][] = array('executeRabbit');
+    //$events[KernelEvents::REQUEST][] = array('executeRabbit');
+    $events[KernelEvents::REQUEST][] = array('rabbitLauncher');
     return $events;
   }
 }
