20a0220d223e47eb782a3b7aae2e66e0a8413b6a
Saphyel <Saphyel@gmail.com>
2016-03-21 16:15:49 +0000

    PLAT-29 Using dependecyinjection

--- profiles/cr/modules/custom/cr_rabbitmq/src/Form/EsuForm.php
+++ profiles/cr/modules/custom/cr_rabbitmq/src/Form/EsuForm.php
@@ -16,23 +16,9 @@ use Drupal\Core\Form\FormStateInterface;
 class EsuForm extends FormBase {
 
   /**
-   * The default queue, handled by Beanstalkd.
-   *
-   * @var \Drupal\beanstalkd\Queue\BeanstalkdQueue
+   * rabbitmq.
    */
-  protected $queue;
-  /**
-   * The queue factory service.
-   *
-   * @var \Drupal\Core\Queue\QueueFactory
-   */
-  protected $queueFactory;
-  /**
-   * Server factory.
-   *
-   * @var \Drupal\rabbitmq\Connection
-   */
-  protected $connectionFactory;
+  protected $rabbit;
   /**
    * {@inheritdoc}
    */
@@ -66,17 +52,8 @@ class EsuForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     drupal_set_message($this->t('Your email address is @email', ['@email' => $form_state->getValue('email')]));
-    //$msg = \Drupal::service('cr_rabbitmq.msg');
-    //$msg->sendmq($form_state->getValue('email'));
-    $name = 'cr';
-    $this->queueFactory = \Drupal::service('queue');
-    $this->queue = $this->queueFactory->get($name);
-    $this->connectionFactory = \Drupal::service('rabbitmq.connection.factory');
-    $connection = $this->connectionFactory->getConnection();
-    $channel = $connection->channel();
-    $data = 'Hello World!';
-    $this->queue->createItem($data);
-    $channel->close();
-    $connection->close();
+
+    $this->rabbit = \Drupal::service('cr_rabbitmq.producer');
+    $this->rabbit->sendMQ($form_state->getValue('email'));
   }
 }
---
+++ profiles/cr/modules/custom/cr_rabbitmq/src/Rabbit/Producer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\cr_rabbitmq\Rabbit;
+
+class Producer {
+  /**
+   * The default queue, handled by Beanstalkd.
+   *
+   * @var \Drupal\beanstalkd\Queue\BeanstalkdQueue
+   */
+  protected $queue;
+  /**
+   * The queue factory service.
+   *
+   * @var \Drupal\Core\Queue\QueueFactory
+   */
+  protected $queueFactory;
+  /**
+   * Server factory.
+   *
+   * @var \Drupal\rabbitmq\Connection
+   */
+  protected $connectionFactory;
+
+  public function sendMQ($data) {
+    $name = 'cr';
+    $this->queueFactory = \Drupal::service('queue');
+    $this->queue = $this->queueFactory->get($name);
+    $this->connectionFactory = \Drupal::service('rabbitmq.connection.factory');
+    $connection = $this->connectionFactory->getConnection();
+    $channel = $connection->channel();
+    $this->queue->createItem($data);
+    $channel->close();
+    $connection->close();
+  }
+}
