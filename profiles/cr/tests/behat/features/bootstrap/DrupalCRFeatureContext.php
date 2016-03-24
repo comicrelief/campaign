5a3d0652990b8a0843dbef425fe523aa74ba4e65
Viresh Patel <viresh.patel@allonesandzeros.co.uk>
2016-03-24 10:02:25 +0000

    PLAT-89 Behat feature context - finished implementation

--- profiles/cr/tests/behat/features/bootstrap/DrupalCRFeatureContext.php
+++ profiles/cr/tests/behat/features/bootstrap/DrupalCRFeatureContext.php
@@ -1,10 +1,11 @@
 <?php
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
+//use Drupal\node\Entity\Node;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
 //use Behat\Behat\Context\Exception\ContextNotFoundException\ContextNotFoundException;
 use Behat\Behat\Tester\Exception\PendingException;
-//use Behat\Testwork\Hook\Scope\BeforeSuiteScope;
 
 /**
  * Defines application features from the specific context.
@@ -13,21 +14,49 @@ class DrupalCRFeatureContext extends RawDrupalContext implements SnippetAcceptin
 
   /**
    * @Then I should see the correct sitemap elements
+   * @And I should see the correct sitemap elements
    */
   public function iShouldSeeTheCorrectSitemapElements()
   {
+    // Grab sitemap.xml page contents and parse it as XML using SimpleXML library
+    $sitemap_contents = $this->getSession()->getDriver()->getContent();
     try {
-      $str_xml = $this->getSession()->getDriver()->getContent();
-      $domdoc_xml = new DOMDocument();
-      $domdoc_xml->loadXML($str_xml);
-      $xpath = new DOMXpath($domdoc_xml);
-      $elements = $xpath->query("/urlset/url");
-
-      if (is_null($elements)) {
-        throw new InvalidArgumentException('No urlset found');
-      }
+      $xml = new SimpleXMLElement($sitemap_contents);
     } catch(Exception $e) {
-      throw new PendingException();
+      throw new Exception('Unable to read sitemap xml content - '.$e->getMessage());
+    }
+
+    // check if <url> nodes exist
+    if (!($xml->count() > 0 && isset($xml->url))) {
+      throw new InvalidArgumentException('No urlset found');
+    }
+  }
+
+  /**
+   * @Then I should see :url_path as a sitemap url
+   * @And I should see :url_path as a sitemap url
+   */
+  public function iShouldSeeAsASitemapUrl($url_path)
+  {
+    // Grab sitemap.xml page contents and parse it as XML using SimpleXML library
+    $sitemap_contents = $this->getSession()->getDriver()->getContent();
+    try {
+      $xml = new SimpleXMLElement($sitemap_contents);
+    } catch(Exception $e) {
+      throw new Exception('Unable to read sitemap xml content - '.$e->getMessage());
+    }
+
+    // Parse through each <url> node and check if url paths provided exist or not
+    $path_found = false;
+    foreach ($xml->children() as $xml_node) {
+      if ( strpos($xml_node->loc, $url_path) ) {
+        $path_found = true;
+      }
+    }
+
+    // If no match found then throw exception
+    if (!$path_found) {
+      throw new InvalidArgumentException('Url not found');
     }
   }
 
