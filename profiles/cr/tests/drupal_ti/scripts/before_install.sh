76ea5a026696471f785f03054d8dccdab75546cf
Zach Bimson <zach.bimson@gmail.com>
2016-04-01 16:53:22 +0100

    PLAT-92 Add module dir and more debug

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -2,4 +2,4 @@
 # Generate build.properties file on the fly
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
 # Output confirmation
-echo 'build.properties has been created.';
+echo 'build.properties has been created.'
