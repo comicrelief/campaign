4526f6f6557fd235df753c3a8e2c1f8b7f574cf2
Zach Bimson <zach.bimson@gmail.com>
2016-04-01 16:36:30 +0100

    PLAT-92 Debug and build.prop confirmation

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Generate build.properties file on the fly
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
+# Output confirmation
+echo 'build.properties has been created.';
