5c6fcee06fa62eb617666f0a397fccca13cbaeba
Zach Bimson <zach.bimson@gmail.com>
2016-04-02 00:30:58 +0100

    PLAT-92 Add info tags

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -2,4 +2,4 @@
 # Generate build.properties file on the fly
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
 # Output confirmation
-echo 'File: build.properties has been created.'
+echo '<info>File: build.properties has been created.</info>'
