3f1093838ab9776c30b787277951d2182ccb3c54
Zach Bimson <zach.bimson@gmail.com>
2016-04-02 00:02:27 +0100

    PLAT-92 Remove old code and debug

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export DRUPAL_TI_MODULES_PATH="modules"
 # Generate build.properties file on the fly
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
 # Output confirmation
