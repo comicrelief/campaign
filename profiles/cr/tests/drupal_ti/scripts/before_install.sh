f13c81408e95ab5022da308bf1307e02d0224406
Zach Bimson <zach.bimson@gmail.com>
2016-04-02 02:16:02 +0100

    PLAT-92 Static path

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -3,5 +3,5 @@
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
 # Output confirmation
 echo 'File: build.properties has been created.'
-cp -R . ../"$DRUPAL_TI_DRUPAL_DIR"
-cd ../"$DRUPAL_TI_DRUPAL_DIR"
+cp -R . /home/travis/build/drupal-8/drupal
+cd /home/travis/build/drupal-8/drupal
