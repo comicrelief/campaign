5f2aa309b82a846a85db3db0c6df78c2f57de48f
Zach Bimson <zach.bimson@gmail.com>
2016-04-02 01:24:10 +0100

    PLAT-92 Before script debug

--- profiles/cr/tests/drupal_ti/scripts/before_script.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_script.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 # Simple script to install drupal for travis-ci running.
 export DRUPAL_TI_MODULES_PATH="debug"
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+if [ -d "$DRUPAL_TI_DRUPAL_DIR" ]
+then
+	return
+fi
+
+# HHVM env is broken: https://github.com/travis-ci/travis-ci/issues/2523.
+PHP_VERSION=`phpenv version-name`
+if [ "$PHP_VERSION" = "hhvm" ]
+then
+	# Create sendmail command, which links to /bin/true for HHVM.
+	BIN_DIR="$TRAVIS_BUILD_DIR/../drupal_travis/bin"
+	mkdir -p "$BIN_DIR"
+	ln -s $(which true) "$BIN_DIR/sendmail"
+	export PATH="$BIN_DIR:$PATH"
+fi
+
+# Clear caches and run a web server.
+drupal_ti_clear_caches
+drupal_ti_run_server
+
+# Start xvfb and selenium.
+drupal_ti_ensure_xvfb
+drupal_ti_ensure_webdriver
