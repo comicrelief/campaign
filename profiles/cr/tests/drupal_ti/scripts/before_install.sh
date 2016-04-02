2bce14eea84ae11265e3f891e3355c3f8c7ad322
Zach Bimson <zach.bimson@gmail.com>
2016-04-02 02:26:05 +0100

    PLAT-92 Updated paths

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -4,6 +4,6 @@ printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
 # Output confirmation
 echo 'File: build.properties has been created.'
 echo "$DRUPAL_TI_DRUPAL_DIR"
-mkdir /home/travis/build/drupal-8/drupal
-cp -R . /home/travis/build/drupal-8/drupal
+mkdir /home/travis/build/drupal-8
+cp -a /home/travis/build/comicrelief/campaign/. /home/travis/build/drupal-8/drupal/
 cd /home/travis/build/drupal-8/drupal
