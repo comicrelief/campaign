03d92545e5bcb7122415b2546aa5af012e4b471b
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-04-14 13:54:32 +0100

    PLAT-1 update gruntfile styleguide tasks

--- profiles/cr/themes/custom/campaign_base/Gruntfile.js
+++ profiles/cr/themes/custom/campaign_base/Gruntfile.js
@@ -5,11 +5,12 @@ module.exports = function (grunt) {
   grunt.initConfig({
     watch: {
       options: {
-        livereload: true
+        livereload: true,
+        nospawn : true
       },
       sass: {
         files: ['sass/{,**/}*.{scss,sass}'],
-        tasks: ['compass:dev'],
+        tasks: ['compass:dev','shell:styleguide'],
         options: {
           livereload: false
         }
@@ -41,6 +42,12 @@ module.exports = function (grunt) {
         src: ['vendor/{,**/}*.js'],
         dest: 'js/vendor.js',
       },
+    },
+
+    shell: {
+        styleguide: {
+            command: 'node_modules/kss/bin/kss-node --source sass/components/ --css http://campaign.local/profiles/cr/themes/custom/campaign_base/css/styles.css --verbose'
+        }
     },
 
     // accessibility: { // todo!!!
@@ -142,6 +149,9 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-browser-sync');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-kss');
+  grunt.loadNpmTasks('grunt-shell');
+
   // grunt.loadNpmTasks('grunt-accessibility');
 
   // grunt.registerTask('test',  [
@@ -149,10 +159,12 @@ module.exports = function (grunt) {
   //   'browserSync:dev'//, 
   //   // 'nodeunit'
   //   ]);
-  grunt.registerTask('default', ['uglify:dev', 'watch']);
+  grunt.registerTask('style', ['shell:styleguide']);
+  grunt.registerTask('default', ['shell:styleguide', 'uglify:dev', 'watch']);
 
   grunt.registerTask('build', [
     // 'uglify:dist',
+    'shell:styleguide',
     'concat',
     'uglify:dist', // todo error when compress
     'compass:dist' //,
