cd129105d010391c56e90b2d82a96f09c685c64a
Peter Vanhee <peter.vanhee@gmail.com>
2016-04-14 14:18:40 +0100

    Feature/plat 160 update contrib (#162)
    * PLAT-160 Adapt build.xml to not write into info files
    
    * PLAT-160 Re-make contrib using `phing make`
    
    * PLAT-160 Fixing paragraph id

--- profiles/cr/modules/contrib/file_entity/src/Form/FileSettingsForm.php
+++ profiles/cr/modules/contrib/file_entity/src/Form/FileSettingsForm.php
@@ -5,14 +5,15 @@
  */
 
 namespace Drupal\file_entity\Form;
-use Drupal\Core\Form\FormBase;
+
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class FileSettingsForm
  * @package Drupal\file_entity\Form
  */
-class FileSettingsForm extends FormBase {
+class FileSettingsForm extends ConfigFormBase {
 
   /**
    * Returns a unique string identifying the form.
@@ -27,13 +28,22 @@ class FileSettingsForm extends FormBase {
   /**
    * {@inheritdoc}
    */
+  protected function getEditableConfigNames() {
+    return [
+      'file_entity.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['max_filesize'] = array(
       '#type' => 'textfield',
       '#title' => t('Maximum upload size'),
       '#default_value' => \Drupal::config('file_entity.settings')->get('max_filesize'),
       '#description' => t('Enter a value like "512" (bytes), "80 KB" (kilobytes) or "50 MB" (megabytes) in order to restrict the allowed file size. If left empty the file sizes will be limited only by PHP\'s maximum post and file upload sizes (current max limit <strong>%limit</strong>).', array('%limit' => format_size(file_upload_max_size()))),
-      '#element_validate' => '\Drupal\file\Plugin\Field\FieldType\FileItem::validateMaxFilesize',
+      '#element_validate' => ['\Drupal\file\Plugin\Field\FieldType\FileItem::validateMaxFilesize'],
       '#size' => 10,
     );
 
@@ -42,7 +52,7 @@ class FileSettingsForm extends FormBase {
       '#title' => t('Default allowed file extensions'),
       '#default_value' => \Drupal::config('file_entity.settings')->get('default_allowed_extensions'),
       '#description' => t('Separate extensions with a space or comma and do not include the leading dot.'),
-      '#element_validate' => '\Drupal\file\Plugin\Field\FieldType\FileItem::validateExtensions',
+      '#element_validate' => ['\Drupal\file\Plugin\Field\FieldType\FileItem::validateExtensions'],
       '#maxlength' => NULL,
     );
 
