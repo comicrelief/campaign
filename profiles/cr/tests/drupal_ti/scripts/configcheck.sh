4f33b334b31001013ae0fc4fc7918d62dc6bb1da
Peter Vanhee <peter.vanhee@gmail.com>
2016-04-15 17:15:12 +0100

    PLAT-165 Stashing changes before doing config:export

--- profiles/cr/tests/drupal_ti/scripts/configcheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/configcheck.sh
@@ -4,18 +4,10 @@
 set -e $DRUPAL_TI_DEBUG
 
 git status
+git stash
 
 phing config:export
 
 git diff --exit-code
 
-# CODE_PATH="profiles/cr/modules/custom"
-# STANDARD="--standard=Drupal"
-# EXTENSIONS="--extensions=php,module,inc,install,test,profile,theme"
-
-# cd "$DRUPAL_TI_BEHAT_DIR"
-# vendor/bin/phpcs --config-set installed_paths "$(pwd)/vendor/drupal/coder/coder_sniffer"
-# cd "$TRAVIS_BUILD_DIR"
-# $DRUPAL_TI_BEHAT_DIR/vendor/bin/phpcs $STANDARD $EXTENSIONS "$CODE_PATH" --runtime-set ignore_warnings_on_exit 1
-# $DRUPAL_TI_BEHAT_DIR/vendor/bin/phpmd "$CODE_PATH" text codesize,unusedcode,naming
-# $DRUPAL_TI_BEHAT_DIR/vendor/bin/phpcpd "$CODE_PATH"
+git stash apply
