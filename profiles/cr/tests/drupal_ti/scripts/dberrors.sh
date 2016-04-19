bc6aace3f2dab4a69f2a7f318b6a7bb493cc6d64
Saphyel <Saphyel@gmail.com>
2016-04-19 12:42:38 +0100

    PLAT-97 fix bash script

--- profiles/cr/tests/drupal_ti/scripts/dberrors.sh
+++ profiles/cr/tests/drupal_ti/scripts/dberrors.sh
@@ -8,14 +8,11 @@ drush wd-show --severity=warning >> tmp.txt
 drush wd-show --severity=error >> tmp.txt
 
 FILESIZE=$(cat tmp.txt | wc -c)
-echo $FILESIZE
+cat tmp.txt
 
-if $FILESIZE -ne 0; then
+if [[ "$FILESIZE" -ne 0 ]]; then
   rm -rf tmp.txt
-  echo "entra"
   exit 1
-else
-  echo "sale"
 fi
 
 rm -rf tmp.txt
