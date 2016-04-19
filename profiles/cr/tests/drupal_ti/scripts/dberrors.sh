f483528d63b78b80ca078f18c91dab7150113991
Saphyel <Saphyel@gmail.com>
2016-04-19 15:49:22 +0100

    PLAT-97 sorting it

--- profiles/cr/tests/drupal_ti/scripts/dberrors.sh
+++ profiles/cr/tests/drupal_ti/scripts/dberrors.sh
@@ -8,9 +8,9 @@ drush wd-show --severity=warning >> tmp.txt
 drush wd-show --severity=error >> tmp.txt
 
 FILESIZE=$(cat tmp.txt | wc -c)
-cat tmp.txt
 
 if [ $FILESIZE -ne 0 ] ; then
+  cat tmp.txt
   rm -rf tmp.txt
   exit 1
 fi
