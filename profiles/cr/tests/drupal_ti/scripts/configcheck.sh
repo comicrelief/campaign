c008d007be47f1301637a33d4466ecd0a42f9116
Peter Vanhee <peter.vanhee@gmail.com>
2016-04-21 11:54:48 +0100

    PLAT-165 Moving stuff around

--- profiles/cr/tests/drupal_ti/scripts/configcheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/configcheck.sh
@@ -7,8 +7,6 @@ set -e $DRUPAL_TI_DEBUG
 git config --global user.email "travis@example.com"
 git config --global user.name "Travis CI"
 
-~/.composer/vendor/bin/drush.php pml
-
 git status
 
 # Stash our changes to settings.php and possibly any other files
@@ -26,7 +24,7 @@ ls -l sites/default
 phing config:export
 
 echo "GIT diffing exluding settings.php"
-git diff `git status -s |grep -v ^\ D |grep -v sites/default/settings.php |cut -b4-` --exit-code
+git diff --exit-code `git status -s |grep -v ^\ D |grep -v sites/default/settings.php |cut -b4-`
 
 
 # cp sites/default/settings.php sites/default/settings.tmp.php
