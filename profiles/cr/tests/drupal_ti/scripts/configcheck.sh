600f596eb033da390014794deae091603e0067c4
Peter Vanhee <peter.vanhee@gmail.com>
2016-04-21 10:52:18 +0100

    PLAT-165 More trials

--- profiles/cr/tests/drupal_ti/scripts/configcheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/configcheck.sh
@@ -11,22 +11,37 @@ git config --global user.name "Travis CI"
 
 git status
 
-# Stash our changes to settings.php
+# Stash our changes to settings.php and possibly any other files
 chmod 777 sites/default/settings.php
 chmod -R 777 sites/default
-cp sites/default/settings.php sites/default/settings.tmp.php
-git stash
+# cp sites/default/settings.php sites/default/settings.tmp.php
+# git stash
 
 ls -l sites/default
 
 git status
-cp sites/default/settings.tmp.php sites/default/settings.php
+# cp sites/default/settings.tmp.php sites/default/settings.php
 
 # Re-export all config - this should not show any changes!
 phing config:export
 
+echo "GIT diffing exluding settings.php"
+git diff `git status -s |grep -v ^\ D |grep -v sites/default/settings.php |cut -b4-`
+
+
+cp sites/default/settings.php sites/default/settings.tmp.php
+
+git checkout sites/default/settings.php
+
 # Exit if there is a diff
+
+echo "GIT diffing with ALL1"
+
 git diff --exit-code
+
+
+cp sites/default/settings.tmp.php sites/default/settings.php
+
 
 # Re-apply our changes to settings.php
 git stash apply
