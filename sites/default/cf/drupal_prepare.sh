bf00a41ba93af6f037e38954746b95eceb6988dd
Benedict Dodd <mail@bendodd.com>
2016-04-25 13:33:02 +0100

    Separate CCI to reusable config YML files

--- sites/default/cf/drupal_prepare.sh
+++ sites/default/cf/drupal_prepare.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+[ -d campaign ] && cd campaign
+[ -d ~/.bashrc ] && . ~/.bashrc
+
 phing build:prepare
 phing build -Ddrush.bin=drush -Ddb.querystring="$DB_QUERYSTRING"
 phing cron -Ddrush.bin=drush
