69efbd7e904e8dc4608ef7a71b5d47dd2b319302
Peter Vanhee <peter.vanhee@gmail.com>
2016-04-28 12:03:32 +0100

    RND-39 More work on form handler, integrating with rabbitmq

--- modules/custom/rnd_preorder/src/Plugin/YamlFormHandler/RabbitMQYamlFormHandler.php
+++ modules/custom/rnd_preorder/src/Plugin/YamlFormHandler/RabbitMQYamlFormHandler.php
@@ -12,6 +12,8 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Mail\MailManagerInterface;
+use Drupal\Core\Queue\QueueFactory;
+use Drupal\Core\Queue\QueueInterface;
 use Drupal\Core\Render\Markup;
 use Drupal\Core\Utility\Token;
 use Drupal\file\Entity\File;
@@ -34,5 +36,188 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class RabbitMQYamlFormHandler extends YamlFormHandlerBase implements YamlFormHandlerMessageInterface {
+
+
+  protected $queueFactory;
+
+  /**
+   * The configuration object factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * The token handler.
+   *
+   * @var \Drupal\Core\Utility\Token $token
+   */
+  protected $token;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerInterface $logger, QueueFactory $queue_factory, ConfigFactoryInterface $config_factory, Token $token) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $logger);
+    $this->$queueFactory = $queue_factory;
+    $this->configFactory = $config_factory;
+    $this->token = $token;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory')->get('yamlform'),
+      $container->get('queue'),
+      $container->get('config.factory'),
+      $container->get('token')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMessage(YamlFormSubmissionInterface $yamlform_submission) {
+//    $token_data = [
+//        'yamlform' => $yamlform_submission->getYamlForm(),
+//        'yamlform-submission' => $yamlform_submission,
+//        'yamlform-submission-options' => [
+//            'email' => TRUE,
+//            'excluded_inputs' => $this->configuration['excluded_inputs'],
+//            'html' => ($this->configuration['html'] && $this->supportsHtml()),
+//        ],
+//    ];
+//
+//    $message = $this->configuration;
+//    unset($message['excluded_inputs']);
+//
+//    // Replace 'default' values and [tokens] with settings default.
+//    $settings = $this->getConfigurationSettings();
+//    foreach ($settings as $setting_key => $setting) {
+//      if (empty($message[$setting_key]) || $message[$setting_key] == 'default') {
+//        $message[$setting_key] = $setting['default'];
+//      }
+//      $message[$setting_key] = $this->token->replace($message[$setting_key], $token_data);
+//    }
+//
+//    // Trim the message body.
+//    $message['body'] = trim($message['body']);
+//
+//    if ($this->configuration['html'] && $this->supportsHtml()) {
+//      switch ($this->getMailSystemSender()) {
+//        case 'swiftmailer':
+//          // SwiftMailer requires that the body be valid Markup.
+//          $message['body'] = Markup::create($message['body']);
+//          break;
+//      }
+//    }
+
+    $message = 'NULL';
+
+
+
+    return $message;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function sendMessage(array $message) {
+//    // Send mail.
+//    $to = $message['to_mail'];
+//    $from = $message['from_mail'] . ' <' . $message['from_name'] . '>';
+//    $current_langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+//    $this->mailManager->mail('yamlform', 'email.' . $this->getHandlerId(), $to, $current_langcode, $message, $from);
+//
+//    // Log message.
+//    $variables = [
+//        '@from_name' => $message['from_name'],
+//        '@from_mail' => $message['from_mail'],
+//        '@to_mail' => $message['to_mail'],
+//        '@subject' => $message['subject'],
+//    ];
+//    \Drupal::logger('yamlform.email')->notice('@subject sent to @to_mail from @from_name [@from_mail].', $variables);
+
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function resendMessageForm(array $message) {
+//    $element = [];
+//    $element['to_mail'] = [
+//        '#type' => 'email',
+//        '#title' => $this->t('To email'),
+//        '#default_value' => $message['to_mail'],
+//    ];
+//    $element['from_mail'] = [
+//        '#type' => 'email',
+//        '#title' => $this->t('From email '),
+//        '#required' => TRUE,
+//        '#default_value' => $message['from_mail'],
+//    ];
+//    $element['from_name'] = [
+//        '#type' => 'textfield',
+//        '#title' => $this->t('From name'),
+//        '#required' => TRUE,
+//        '#default_value' => $message['from_name'],
+//    ];
+//    $element['subject'] = [
+//        '#type' => 'textfield',
+//        '#title' => $this->t('Subject'),
+//        '#default_value' => $message['subject'],
+//    ];
+//    $body_format = ($this->configuration['html']) ? 'html' : 'text';
+//    $element['body'] = [
+//        '#type' => 'yamlform_codemirror_' . $body_format,
+//        '#title' => $this->t('Message (@format)', ['@format' => ($this->configuration['html']) ? $this->t('HTML') : $this->t('Plain text')]),
+//        '#rows' => 10,
+//        '#required' => TRUE,
+//        '#default_value' => $message['body'],
+//    ];
+//    $element['html'] = [
+//        '#type' => 'value',
+//        '#value' => $message['html'],
+//    ];
+//    $element['attachments'] = [
+//        '#type' => 'value',
+//        '#value' => $message['attachments'],
+//    ];
+//
+//    // Display attached files.
+//    if ($message['attachments']) {
+//      $file_links = [];
+//      foreach ($message['attachments'] as $attachment) {
+//        $file_links[] = [
+//            '#theme' => 'file_link',
+//            '#file' => $attachment['file'],
+//            '#prefix' => '<div>',
+//            '#suffix' => '</div>',
+//        ];
+//      }
+//      $element['files'] = [
+//          '#type' => 'item',
+//          '#title' => $this->t('Attachments'),
+//          '#markup' => \Drupal::service('renderer')->render($file_links),
+//      ];
+//    }
+//
+//    return $element;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getMessageSummary(array $message) {
+    return [
+        '#settings' => $message,
+    ] + parent::getSummary();
+  }
 
 }
