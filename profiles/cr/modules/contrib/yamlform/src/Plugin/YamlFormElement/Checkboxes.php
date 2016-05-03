b4a96f3bf7303e431f5d649dae32124dd8b5d2ac
Saphyel <c.marin@comicrelief.com>
2016-05-03 12:31:12 +0100

    RND-39 Execute make

---
+++ profiles/cr/modules/contrib/yamlform/src/Plugin/YamlFormElement/Checkboxes.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\yamlform\Plugin\YamlFormElement\Checkboxes.
+ */
+
+namespace Drupal\yamlform\Plugin\YamlFormElement;
+
+/**
+ * Provides a 'checkboxes' element.
+ *
+ * @YamlFormElement(
+ *   id = "checkboxes",
+ *   label = @Translation("Checkboxes")
+ * )
+ */
+class Checkboxes extends OptionsBase {
+
+}
