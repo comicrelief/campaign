d743e9baaabed79dc5cd0880af7d34161e227a4e
Andy Phipps <a.phipps@comicrelief.com>
2016-05-18 12:21:19 +0100

    PLAT-232: JS setup

--- profiles/cr/modules/custom/cr_email_signup/js/cr_email_signup.js
+++ profiles/cr/modules/custom/cr_email_signup/js/cr_email_signup.js
@@ -3,18 +3,20 @@
  * Javascript functionality for the ESU strip
  */
 
-
 (function($, Drupal) {
   'use strict';
 
-  console.log("i live");
+  Drupal.behaviors.crEMailSignup = {
 
-
-  Drupal.behaviors.myBehavior = {
     attach: function (context, settings) {
+      var _base = Drupal.behaviors.crEMailSignup;
+      $('#cr-email-signup-form', context).once('crEMailSignup', _base.handleEmailSubmit()).addClass('processed');
+    },
 
-      $(context).find('#cr-email-signup-form').once('myCustomBehavior').addClass('processed').addClass('XXX');
-   
+    handleEmailSubmit: function (context) {
+      $(document).on('click', '#edit-send-email', function(){
+        alert("success");
+      });
     }
   };
 })(jQuery, Drupal);
