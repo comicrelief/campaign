ea3ab466490dd7645bb54f65b350cfec2923e28c
Zach Bimson <zach.bimson@gmail.com>
2016-05-26 10:50:31 +0100

    PLAT-165 Trials

--- profiles/cr/tests/drupal_ti/scripts/configcheck.sh
+++ profiles/cr/tests/drupal_ti/scripts/configcheck.sh
@@ -10,11 +10,10 @@ git config --global user.name "Travis CI"
 # Re-export all config - this should not show any changes!
 phing config:export
 
-
 chmod 777 sites/default/settings.php
 
 # mv sites/default/settings.php settings.php.tmp
-git checkout sites/default/settings.php
+sudo git checkout sites/default/settings.php
 
 git status
 
@@ -27,7 +26,6 @@ cat git-diff.txt
 
 # mv sites/default/settings.php.tmp sites/default/settings.php
 
-
 FILESIZE=$(cat git-diff.txt | wc -c)
 
 if [ $FILESIZE -ne 0 ] ; then
@@ -36,7 +34,7 @@ if [ $FILESIZE -ne 0 ] ; then
   exit 1
 fi
 
+cat git-diff.txt
 rm -rf git-diff.txt
-
 
 echo "Reached end of file"
