2148e13ef10f55d3aaed8bec78a32b96889e4186
Zach Bimson <zach.bimson@gmail.com>
2016-06-14 14:48:56 +0100

    RND-171 Update select box ui

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -1,1 +1,6 @@
-// "use strict";
+(function($, Drupal) {
+  $( document ).ready(function() {
+    // Turn our boring select boxes into sexy jQuery UI selectboxes
+    $('select').selectmenu({ style:'popup', width: '100%' });
+  });
+})(jQuery, Drupal);
