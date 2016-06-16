62bab98521fd8e43d0cd9991f5cc2849e38faed4
Saphyel <c.marin@comicrelief.com>
2016-06-16 14:16:07 +0100

    PLAT-291 fix format

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,29 +1,28 @@
 <?php
-use Symfony\Component\Yaml\Yaml;
 
+use Symfony\Component\Yaml\Yaml;
 
 $settings['install_profile'] = 'cr';
 $settings['skip_permissions_hardening'] = TRUE;
 $config_directories = array();
 $settings['update_free_access'] = FALSE;
-$settings['allow_authorize_operations'] = FALSE;
-$settings['session_write_interval'] = 180;
 $settings['file_chmod_directory'] = 0775;
 $settings['file_chmod_file'] = 0664;
-$settings['class_loader_auto_detect'] = TRUE;
-$settings['omit_vary_cookie'] = TRUE;
-# $settings['deployment_identifier'] = \Drupal::VERSION;
+//$settings['allow_authorize_operations'] = FALSE;
+//$settings['session_write_interval'] = 180;
+//$settings['class_loader_auto_detect'] = TRUE;
+//$settings['omit_vary_cookie'] = TRUE;
 
 /**
  * Load environment variables.
  */
 $environment = __DIR__ . "/environment.yml";
 if (file_exists($environment)) {
-	$environment_variables = Yaml::parse(file_get_contents($environment));
+  $environment_variables = Yaml::parse(file_get_contents($environment));
 
-	$databases = $environment_variables['databases'];
-	$settings = array_merge($settings, $environment_variables['settings']);
-	$config = array_merge($config, $environment_variables['config']);
+  $databases = $environment_variables['databases'];
+  $settings = array_merge($settings, $environment_variables['settings']);
+  $config = array_merge($config, $environment_variables['config']);
 }
 
 /**
@@ -42,5 +41,5 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  */
 
 if (getenv('VCAP_SERVICES')) {
-    include __DIR__ . "/settings.cf.php";
+  include __DIR__ . "/settings.cf.php";
 }
