ffd5353645f07e475a4e12330474efa0750af456
Saphyel <c.marin@comicrelief.com>
2016-06-28 12:22:59 +0100

    RND-206 Execute phing

--- profiles/cr/modules/contrib/focal_point/src/Plugin/Field/FieldWidget/FocalPointImageWidget.php
+++ profiles/cr/modules/contrib/focal_point/src/Plugin/Field/FieldWidget/FocalPointImageWidget.php
@@ -26,7 +26,7 @@ class FocalPointImageWidget extends ImageWidget {
   /**
    * {@inheritDocs}
    *
-   * Form API callback: Processes a image_fp field element.
+   * Form API callback: Processes an image_fp field element.
    *
    * Expands the image_fp type to include the focal_point field.
    *
@@ -117,11 +117,17 @@ class FocalPointImageWidget extends ImageWidget {
    * Validate callback for the focal point field.
    */
   public static function validateFocalPoint($element, FormStateInterface $form_state) {
-    $field_name = array_pop($element['#parents']);
-    $focal_point_value = $form_state->getValue($field_name);
+    $field_name = array_shift($element['#parents']);
+    $field_values = $form_state->getValue($field_name);
 
-    if (!is_null($focal_point_value) && \Drupal::service('focal_point.manager')->validateFocalPoint($focal_point_value)) {
-      $form_state->setError($element, new TranslatableMarkup('The !title field should be in the form "leftoffset,topoffset" where offsets are in percents. Ex: 25,75.', array('!title' => $element['#title'])));
+    if (!is_null($field_values)) {
+      foreach ($field_values as $field_value) {
+        $focal_point_value = $field_value['focal_point'];
+
+        if (FALSE === \Drupal::service('focal_point.manager')->validateFocalPoint($focal_point_value)) {
+          $form_state->setError($element, new TranslatableMarkup('The !title field should be in the form "leftoffset,topoffset" where offsets are in percents. Ex: 25,75.', array('!title' => $element['#title'])));
+        }
+      }
     }
   }
 
