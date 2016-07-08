edb5eb07b0b67f8f4922ada817221a01c446111d
Peter Vanhee <peter.vanhee@gmail.com>
2016-07-08 12:44:40 +0100

    Do not run gems on systest

--- sites/default/cf/drupal_systest.sh
+++ sites/default/cf/drupal_systest.sh
@@ -9,5 +9,5 @@ export DRUPAL_ROOT=$(pwd)
 export DRUSH_ALIAS='@self'
 export BEHAT_PARAMS='{"extensions":{"Behat\\MinkExtension":{"base_url":"'$BASE_URL'"},"Drupal\\DrupalExtension":{"drupal":{"drupal_root":"'$DRUPAL_ROOT'"},"drush":{"alias":"'$DRUSH_ALIAS'"}}}}'
 
-phing build:prepare
+phing build:prepare:no-gems
 phing test -Dapp.uri="$BASE_URL" -Dapp.dir=$(pwd)
