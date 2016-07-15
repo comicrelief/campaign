346f5bfb7ed4371b1288918c2156bb69fe452baa
Andy Phipps <a.phipps@comicrelief.com>
2016-07-15 16:16:01 +0100

    PLAT-323: basic JS intergraton for touch menu, styles needs lots of work, JS needs to be module-ised

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -2,5 +2,22 @@
   $( document ).ready(function() {
     // Turn our boring select boxes into sexy jQuery UI selectboxes
     $('select').selectmenu({ style:'popup', width: '100%' });
+
+
+
+    // TODO: implemented as Drupal behaviour within navigation module
+    $('.menu--main .menu-item--expanded > a').on('click', function(e){
+
+      // Prevent our parent item from acting as link, user
+      // able to link to parent content with duplicate link within the ul (hook to be developed)
+      e.preventDefault();
+
+      // Close any open nav items
+      $('.menu--main .menu-item--expanded > ul.menu').removeClass('active');
+
+      // Active the selected menu
+      $(this).next('ul.menu').addClass('active');
+    });
+
   });
 })(jQuery, Drupal);
