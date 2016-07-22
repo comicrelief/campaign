ebc93f179c4dfa69944b7c89dda4a36d87b3f809
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-07-22 12:31:21 +0100

    PLAT-322 add lightcase call

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -2,5 +2,7 @@
   $( document ).ready(function() {
     // Turn our boring select boxes into sexy jQuery UI selectboxes
     $('select').selectmenu({ style:'popup', width: '100%' });
+    // Activate lighcase
+    $('a[rel^=lightcase]').lightcase();
   });
 })(jQuery, Drupal);
