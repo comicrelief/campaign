34628d770e83981ddbb12f2b89188e18812d474a
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-07-27 16:05:15 +0100

    PLAT-366 cw js

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -3,4 +3,63 @@
     // Turn our boring select boxes into sexy jQuery UI selectboxes
     $('select').selectmenu({ style:'popup', width: '100%' });
   });
+
+  // Content wall body top position 
+  Drupal.behaviors.contentWall = {
+    attach: function (context, settings) {
+
+      sSize = $(window).width();
+
+      $('.cw-article__body--hide-show', context).once('cw-hide-show', function () {
+        
+        $(window).load(function() {
+          if (sSize >= 768) {
+            newPosition();
+            newHeight();
+          }
+          if (sSize >= 1400) {
+            $('.media-video').closest('.cw-article').addClass('cw-article-media-video');
+          }
+        });
+
+        $(window).resize(function() {          
+          sSize = $(window).width();
+          if (sSize >= 768 || sSize == 992 || sSize == 1400) {
+            newPosition();
+            newHeight();
+          }
+          if (sSize >= 1400) {
+            $('.media-video').closest('.cw-article').addClass('cw-article-media-video');
+          }
+        });
+
+        var newHeight = function() {
+          $('.cw-article__body--hide-show').each(function(){
+            var aHeight = $(this).height();
+            var hHeight = $(this).find('.cw-article__title').height();
+            var nHeight = aHeight - hHeight + 5;
+            $(this).parent().find('.cw-article__image').css('height',nHeight+'px');
+          });
+        }
+
+        var newPosition = function() {
+          $('.cw-article__body--hide-show').each(function(){
+            var aHeight = $(this).height();
+            var hHeight = $(this).find('.cw-article__title').height();
+            var topPos = aHeight - hHeight;
+            if (topPos > 167 && sSize < 992) {
+              $(this).css('top','167px');
+            } else if (topPos > 229 && sSize < 1400) {
+              $(this).css('top','229px');
+            } else if (topPos > 335) {
+              $(this).css('top','335px');
+            } else {
+              $(this).css('top',topPos+'px');
+            }
+          });
+        }
+
+      });
+    }
+  };
 })(jQuery, Drupal);
