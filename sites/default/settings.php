4fdaa1d023495398147e71b5b33613e6eaad8be3
Peter Vanhee <peter.vanhee@gmail.com>
2016-07-29 00:32:42 +0100

    Add local settings reference needed for platform.sh

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -25,15 +25,16 @@ if (file_exists($environment)) {
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
- * Include the Pantheon-specific settings file.
- *
- * n.b. The settings.pantheon.php file makes some changes
- *      that affect all envrionments that this site
- *      exists in.  Always include this file, even in
- *      a local development environment, to insure that
- *      the site settings remain consistent.
+ * Include the Cloud Foundry-specific settings file.
  */
-
 if (getenv('VCAP_SERVICES')) {
   include __DIR__ . "/settings.cf.php";
+}
+
+/**
+ * Include settings for platform.sh
+ */
+$local_settings = dirname(__FILE__) . '/settings.local.php';
+if (file_exists($local_settings)) {
+  require $local_settings;
 }
