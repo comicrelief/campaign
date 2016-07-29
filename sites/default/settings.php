04ab2b306159f21312af99bd855aa122bf5f98d0
Peter Vanhee <peter.vanhee@gmail.com>
2016-07-29 11:40:24 +0100

    Fix

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -25,6 +25,13 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  */
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
+// Set up a config sync directory.
+//
+// This is defined inside the read-only "config" directory. This works well,
+// however it requires a patch from issue https://www.drupal.org/node/2607352
+// to fix the requirements check and the installer.
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
+
 /**
  * Include settings for platform.sh
  */
