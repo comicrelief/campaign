ebbbdfb89eaf8dd309a1887508840a1d23310a90
Peter Vanhee <peter.vanhee@gmail.com>
2016-07-29 10:00:40 +0100

    Undo previous commits, use phing

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,42 +1,33 @@
 <?php
+
+use Symfony\Component\Yaml\Yaml;
+
+$databases = array();
+$config_directories = array();
+$settings['install_profile'] = 'cr';
+
 /**
- * @file
- * Platform.sh example settings.php file for Drupal 8.
+ * Load environment variables.
  */
-// Default Drupal 8 settings.
-//
-// These are already explained with detailed comments in Drupal's
-// default.settings.php file.
-//
-// See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
-$databases = [];
-$config_directories = [];
-$settings['update_free_access'] = FALSE;
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
-// Install with the 'standard' profile for this example.
-//
-// As the settings.php file is not writable during install on Platform.sh (for
-// good reasons), Drupal will refuse to install a profile that is not defined
-// here.
-$settings['install_profile'] = 'standard';
-// The hash_salt should be a unique random value for each application.
-// If left unset, the settings.platformsh.php file will attempt to provide one.
-// You can also provide a specific value here if you prefer and it will be used
-// instead. In most cases it's best to leave this blank on Platform.sh. You
-// can configure a separate hash_salt in your settings.local.php file for
-// local development.
-// $settings['hash_salt'] = 'change_me';
-// Set up a config sync directory.
-//
-// This is defined inside the read-only "config" directory. This works well,
-// however it requires a patch from issue https://www.drupal.org/node/2607352
-// to fix the requirements check and the installer.
-// $config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
-// Automatic Platform.sh settings.
-if (file_exists(__DIR__ . '/settings.platformsh.php')) {
-  include __DIR__ . '/settings.platformsh.php';
+$environment = __DIR__ . "/environment.yml";
+if (file_exists($environment)) {
+  $environment_variables = Yaml::parse(file_get_contents($environment));
+
+  $databases = $environment_variables['databases'];
+  $settings = array_merge($settings, $environment_variables['settings']);
+  $config = array_merge($config, $environment_variables['config']);
+  $config_directories['sync'] = $environment_variables['config_dir'];
 }
-// Local settings. These come last so that they can override anything.
-if (file_exists(__DIR__ . '/settings.local.php')) {
-  include __DIR__ . '/settings.local.php';
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include settings for platform.sh
+ */
+$local_settings = dirname(__FILE__) . '/settings.local.php';
+if (file_exists($local_settings)) {
+  require $local_settings;
 }
