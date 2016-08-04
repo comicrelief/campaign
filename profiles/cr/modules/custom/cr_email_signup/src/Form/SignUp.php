594e16fb844cf9c269a4dcb5962974217e0a1378
Tom Staunton <t.staunton@comicrelief.com>
2016-08-04 11:07:48 +0100

    RND-178: Coding Standards

--- profiles/cr/modules/custom/cr_email_signup/src/Form/SignUp.php
+++ profiles/cr/modules/custom/cr_email_signup/src/Form/SignUp.php
@@ -51,12 +51,14 @@ class SignUp extends FormBase {
     // RND-178: Device & Source Replacements.
     if (!empty($append_message['device'])) {
       $append_message['transSource'] = str_replace("[Device]", $append_message['device'], $append_message['transSource']);
-    } else {
+    }
+    else {
       $append_message['transSource'] = str_replace("[Device]", "Unknown", $append_message['transSource']);
     }
     if (!empty($append_message['source'])) {
       $append_message['transSource'] = str_replace("[PageElementSource]", $append_message['source'], $append_message['transSource']);
-    } else {
+    }
+    else {
       $append_message['transSource'] = str_replace("[PageElementSource]", "Unknown", $append_message['transSource']);
     }
 
@@ -107,14 +109,14 @@ class SignUp extends FormBase {
       '#type' => 'hidden',
       '#attributes' => array(
         'id' => 'esu-device',
-      )
+      ),
     ];
     $form['steps']['source'] = [
       '#name' => 'source',
       '#type' => 'hidden',
       '#attributes' => array(
         'id' => 'esu-source',
-      )
+      ),
     ];
 
     $form['steps']['step1'] = [
