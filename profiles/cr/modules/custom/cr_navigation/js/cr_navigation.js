75bea4a1b92b7bff3f4258b566b769185dd58852
Andy Phipps <a.phipps@comicrelief.com>
2016-08-04 17:24:19 +0100

    PLAT-323: JS tidy up with new functions

--- profiles/cr/modules/custom/cr_navigation/js/cr_navigation.js
+++ profiles/cr/modules/custom/cr_navigation/js/cr_navigation.js
@@ -1,16 +1,9 @@
 (function ($) {
 
   Drupal.behaviors.crNavigation = {
-
    settings : {
     mainMenuClass: '.menu--main',
     navItemWithSubMenuSelector:'.menu--main > .menu > .menu-item--expanded',
-
-    subMenuSelector : '.menu--main .menu-item--expanded > ul.menu',
-    touchNavBreakpoint : '(max-width: 1149px)',
-    noneTouchNavBreakpoint : '(min-width: 1150px)',
-    isTouchDevice : false,
-    isTouchNav : false,
    },
 
     attach: function (context, settings) {
@@ -19,30 +12,39 @@
 
       $(_settings.mainMenuClass).once('crNavigation').each( function(){
         $(this).addClass("crNavigation-processed");
-
-          _base.duplicateParentLink();
-          
-          $('#main-menu').smartmenus({
-            subIndicatorsText: "",
-            keepHighlighted: false
-          });
-
-        _base.toggleMenu();
-
+          _base.setUpNav();
       });
     },
 
-    duplicateParentLink: function (context, settings) {
+    setUpNav: function (context, settings) {
       var _base = Drupal.behaviors.crNavigation;
       var _settings = _base.settings;
 
+      _base.duplicateParentLink();
+
+      _base.toggleMenu();
+
+      /* Setup the Smartmenus plugin with our main menu */
+      $('#main-menu').smartmenus({
+        subIndicatorsText: "",
+        keepHighlighted: false,
+        hideOnClick: true,
+      });
+
+      /* Bind the 'show' function to also hide all the other submenus */
+      $('#main-menu').bind('activate.smapi', function(e, menu) {
+        $('#main-menu').smartmenus('menuHideAll');
+      });
+
+    },
+
+    /* Updates empty duplicate link (added by template) with the parent item's text and link, dynamically */
+    duplicateParentLink: function (context, settings) {
+
       // Update text and link
-      $( _settings.navItemWithSubMenuSelector ).each (function() {
+      $('.menu--main > .menu > .menu-item--expanded').each (function() {
 
         $this = $(this);
-
-        // Add this class so the SmartMenu plugin ignores any clicks, making it function as a button only
-        $this.children('a').addClass('disabled');
 
         // Populate duplicate link with parent link info
         $(this).children('ul.menu')
@@ -52,10 +54,8 @@
       });
     },
 
+    /* Click event handler to show/hide the mobile nav */
     toggleMenu: function (context, settings) {
-
-      var _base = Drupal.behaviors.crNavigation;
-      var _settings = _base.settings;
 
       $('button.main-menu-toggle').on('click', function(e) {
 
@@ -66,91 +66,5 @@
         $('#main-menu').toggleClass('menu-open');
       });
     },
-
-
-
-
-
-    /*init : function () {
-      var _base = Drupal.behaviors.crNavigation;
-      var _settings = _base.settings;
-
-      // Check Modernizr class to assess if we're using a touch device or not
-      _settings.isTouchDevice = $('html').hasClass('touchevents');
-      _base.checkTouchBreakpoint();
-
-      // Runs regardless of breakpoint/touch
-      _base.duplicateParentLink(this);
-      _base.storeHeights(this);
-
-    },
-
-
-
-
-    storeHeights: function (context, settings) {
-      var _base = Drupal.behaviors.crNavigation;
-      var _settings = _base.settings;
-
-      // If we're on the non touch breakpoint, we don't need to do the JS magic for animating, so exit the function
-      if ( _settings.isTouchNav ) {
-        return;
-      }
-
-      // Store height as data-attribute for animating with later, then set max-height to 0
-      $(_settings.subMenuSelector).each( function () {
-        $(this)
-          .attr("data-menu-height", $(this).height())
-            .css("max-height", 0);
-      });
-      _base.animateMenu(this);
-    },
-
-
-    animateMenu: function (context, settings) {
-
-      var _base = Drupal.behaviors.crNavigation;
-      var _settings = _base.settings;
-
-      $('.menu--main .menu-item--expanded > a').on('click', function(e) {
-
-        // Only prevent default when we're on the mobile/touch menu, else exit the function
-        if ( _settings.isTouchNav ) {
-          e.preventDefault();
-        } else {
-          return;
-        }
-      
-        $thisMenu = $(this).next('ul.menu');
-
-        // Set our max-height dynamically based on actual height stored earlier
-        var thisMenuHeight =  $thisMenu.attr("data-menu-height");
-        // Current height of menu
-        var thisCurrentHeight = $thisMenu.height();
-
-        // Update current height to toggle between the 2 values
-        thisCurrentHeight = thisMenuHeight == thisCurrentHeight ? 0 : thisMenuHeight;
-
-        $thisMenu.css("max-height", thisCurrentHeight + "px").toggleClass('active');
-
-        // Close any open nav items
-        $('.menu--main .menu-item--expanded > a')
-          .not(this)
-            .next('ul.menu')
-              .removeClass('active')
-                .css("max-height", 0);
-      });
-    },
-
-    checkTouchBreakpoint : function () {
-
-      var _settings = Drupal.behaviors.crNavigation.settings;
-
-      if ( window.matchMedia('(max-width: 1149px)').matches ) {
-        _settings.isTouchNav = true;
-      } else {
-        _settings.isTouchNav = false;
-      }
-    },*/
   };
 })(jQuery);
