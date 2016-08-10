b611ed4661d81f4b966f77ff29cdfc269c4129ce
Peter Vanhee <peter.vanhee@gmail.com>
2016-08-10 23:31:32 +0100

    Refactoring settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,38 +1,26 @@
 <?php
 
-// use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Yaml;
 
 $databases = [];
 $config_directories = [];
+$settings['install_profile'] = 'cr';
 $settings['update_free_access'] = FALSE;
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
  * Load environment variables.
+ * Required for CRAFT.
  */
-// $environment = __DIR__ . "/environment.yml";
-// if (file_exists($environment)) {
-//   $environment_variables = Yaml::parse(file_get_contents($environment));
+$environment = __DIR__ . "/environment.yml";
+if (file_exists($environment)) {
+  $environment_variables = Yaml::parse(file_get_contents($environment));
 
-//   $databases = $environment_variables['databases'];
-//   $settings = array_merge($settings, $environment_variables['settings']);
-//   $config = array_merge($config, $environment_variables['config']);
-//   $config_directories['sync'] = $environment_variables['config_dir'];
-// }
-
-/**
- * Load services definition file.
- */
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
-
-// Set up a config sync directory.
-//
-// This is defined inside the read-only "config" directory. This works well,
-// however it requires a patch from issue https://www.drupal.org/node/2607352
-// to fix the requirements check and the installer.
-$config_directories[CONFIG_SYNC_DIRECTORY] = '../sync';
-
-$settings['install_profile'] = 'cr';
+  $databases = $environment_variables['databases'];
+  $settings = array_merge($settings, $environment_variables['settings']);
+  $config = array_merge($config, $environment_variables['config']);
+  $config_directories['sync'] = $environment_variables['config_dir'];
+}
 
 /**
  * Include settings for platform.sh
@@ -43,6 +31,7 @@ if (file_exists(__DIR__ . '/settings.platformsh.php')) {
   $config_directories[CONFIG_SYNC_DIRECTORY] = 'sites/default/config';
 }
 // Local settings. These come last so that they can override anything.
+// Also used by platform.sh - not for local development!
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
