0bdb75241bb953bc30c4d5230327a1a11be3dc72
Peter Vanhee <peter.vanhee@gmail.com>
2016-08-10 22:50:33 +0100

    Removing core hacks

--- core/modules/system/src/Tests/Installer/InstallerExistingConfigDirectoryTest.php
+++ core/modules/system/src/Tests/Installer/InstallerExistingConfigDirectoryTest.php
@@ -12,20 +12,11 @@ use Drupal\simpletest\InstallerTestBase;
 class InstallerExistingConfigDirectoryTest extends InstallerTestBase {
 
   /**
-   * The expected file perms of the folder.
-   *
-   * @var int
-   */
-  protected $expectedFilePerms;
-
-  /**
    * {@inheritdoc}
    */
   protected function setUp() {
-    mkdir($this->siteDirectory . '/config_read_only', 0444);
-    $this->expectedFilePerms = fileperms($this->siteDirectory . '/config_read_only');
     $this->settings['config_directories'][CONFIG_SYNC_DIRECTORY] = (object) array(
-      'value' => $this->siteDirectory . '/config_read_only',
+      'value' => $this->siteDirectory . '/config',
       'required' => TRUE,
     );
     parent::setUp();
@@ -37,8 +28,6 @@ class InstallerExistingConfigDirectoryTest extends InstallerTestBase {
   public function testInstaller() {
     $this->assertUrl('user/1');
     $this->assertResponse(200);
-    $this->assertEqual($this->expectedFilePerms, fileperms($this->siteDirectory . '/config_read_only'));
-    $this->assertEqual([], glob($this->siteDirectory . '/config_read_only/*'), 'The sync directory is empty after install because it is read-only.');
   }
 
 }
