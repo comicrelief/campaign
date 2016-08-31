984a6e93b8ea817599803209520d1f2b9496a2da
Saphyel <c.marin@comicrelief.com>
2016-08-31 14:27:06 +0100

    PLAT-375 Add head variations

---
+++ profiles/cr/modules/custom/cr_email_signup/src/Form/HeadEsu.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains HeadESU.php.
+ */
+
+namespace Drupal\cr_email_signup\Form;
+
+
+class HeadEsu extends StandardSignUp {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'cr_header_email_signup_form';
+  }
+
+}
