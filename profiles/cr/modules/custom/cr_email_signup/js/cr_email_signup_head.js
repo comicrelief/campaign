6fe4c5ccdef40cb80fa34e0fdd3cb24c9c11b3f7
Matthew Wagg <mattw@MACPRO-DEV02.local>
2016-09-02 16:08:23 +0100

    plat-375 updated JS to close the ESU box with the cross

--- profiles/cr/modules/custom/cr_email_signup/js/cr_email_signup_head.js
+++ profiles/cr/modules/custom/cr_email_signup/js/cr_email_signup_head.js
@@ -3,5 +3,8 @@
     $("button.main-menu__icons-esu-toggle").on("click", function() {
       $(".block--cr-email-signup--head").toggleClass("show");
     });
+    $(".block--cr-email-signup--head .icon").on("click", function() {
+      $(".block--cr-email-signup--head").removeClass("show");
+    });
   });
 })(jQuery);
