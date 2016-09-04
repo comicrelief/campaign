70804ea64967922ee67e8dd46c59d59d24267b96
Peter Vanhee <peter.vanhee@gmail.com>
2016-09-04 22:26:30 +0100

    PLAT-433 Testing automation of release process

---
+++ profiles/cr/tests/drupal_ti/scripts/create_release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Simple script to automate creation of a release branch for RND17
+
+set -e $DRUPAL_TI_DEBUG
+
+# Only continue if we are on a release branch
+if [[ "$TRAVIS_BRANCH" = "release"* ]]
+then
+  echo "We are on a release branch. Prepare integration into RND17"
+
+  # Check out a new branch or find existing release branch?
+  git clone git@github.com:comicrelief/rnd17.git rnd17
+  cd rnd17
+
+  # Check if we already have a release branch in RND17, if not create a new one
+  if [ `git branch --list $TRAVIS_BRANCH `]
+  then
+    echo "Release branch already exists in RND17. We'll ping this branch so it triggers a rebuild."
+    git checkout $TRAVIS_BRANCH
+  else
+    echo "Release branch created in RND17. We'll push this up so that a build is triggered."
+    git checkout -b $TRAVIS_BRANCH
+    git push origin $TRAVIS_BRANCH
+  fi
+fi
+
