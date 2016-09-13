1a8b12d872868f8eef43cd7a044a7434f1c1a0a5
Andy Phipps <andyp@lt5cg6224xk8.vxh.comicrelief.org.uk>
2016-09-13 16:10:59 +0100

    PLAT-375: tweaking SVG and layout in header ESU

--- profiles/cr/modules/custom/cr_email_signup/js/cr_email_signup_head.js
+++ profiles/cr/modules/custom/cr_email_signup/js/cr_email_signup_head.js
@@ -1,9 +1,11 @@
 (function ($) {
   $(document).ready(function () {
     $("button.main-menu__icons-esu-toggle").on("click", function() {
+      $(this).toggleClass("active");
       $(".block--cr-email-signup--head").toggleClass("show");
     });
     $(".block--cr-email-signup--head .icon").on("click", function() {
+      $("button.main-menu__icons-esu-toggle").removeClass("active");
       $(".block--cr-email-signup--head").removeClass("show");
     });
   });
