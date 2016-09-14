94ca944545c3076a8beb02c7c23c720ab00f1df7
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-09-14 16:23:12 +0100

    PROTO replace title with name url parameter

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -1,6 +1,12 @@
 (function($, Drupal) {
 
   $( document ).ready(function() {
+
+    name = decodeURIComponent($.urlParam('name'));
+
+    console.log(name);
+    $(".page__title").text("Welcome " + name);
+
     // Turn our boring select boxes into sexy jQuery UI selectboxes
     $('select').selectmenu({ style:'popup', width: '100%' });
     // Activate lighcase
@@ -10,6 +16,12 @@
 	  newHeight();
 
   });
+
+  // Show name url att in page title
+  $.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    return results[1] || 0;
+  }
 
   sSize = $(window).width();
 
