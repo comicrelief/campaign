0bffc0e147ba00f1c276b2cbfe14d56760eac598
Zach Bimson <zach.bimson@gmail.com>
2016-09-23 16:06:46 +0100

    PLAT-349 Extend workplace, quickest option

--- profiles/cr/modules/custom/cr_email_signup/src/Plugin/Block/FundraiseSignUpBlock.php
+++ profiles/cr/modules/custom/cr_email_signup/src/Plugin/Block/FundraiseSignUpBlock.php
@@ -16,80 +16,13 @@ use Drupal\Core\Access\AccessResult;
  *   admin_label = @Translation("Email Sign Up block: Fundraise"),
  * )
  */
-class FundraiseSignUpBlock extends BlockBase implements BlockPluginInterface {
+class FundraiseSignUpBlock extends WorkplaceSignUpBlock implements BlockPluginInterface {
 
   /**
    * Return the form.
    */
   protected function getEsuForm() {
     return \Drupal::formBuilder()->getForm('Drupal\cr_email_signup\Form\FundraiseSignUp');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function build() {
-    $config = $this->getConfiguration();
-
-    $form = $this->getEsuForm();
-
-    $messages = array_slice($config, 4);
-    foreach ($messages as $keymsg => $valuemsg) {
-      $classname = 'esu-' . str_replace('_', '-', $keymsg);
-      $form[$keymsg] = [
-        '#markup' => "<div class='$classname'><p>" . $valuemsg . "</p></div>",
-      ];
-    }
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function access(AccountInterface $account, $return_as_object = FALSE) {
-    return AccessResult::allowed();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $form = parent::blockForm($form, $form_state);
-
-    $config = $this->getConfiguration();
-
-    $form['cr_email_signup_title_message'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('title Message'),
-      '#description' => $this->t('Enter the title message to show'),
-      '#default_value' => isset($config['title_message']) ? $config['title_message'] : '',
-    ];
-
-    $form['cr_email_signup_initial_message'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Initial Message'),
-      '#description' => $this->t('Enter the initial message to show'),
-      '#default_value' => isset($config['initial_message']) ? $config['initial_message'] : '',
-    ];
-
-    $form['cr_email_signup_first_success_message'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('First Success Message'),
-      '#description' => $this->t('Enter the success message'),
-      '#default_value' => isset($config['first_success_message']) ? $config['first_success_message'] : '',
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->setConfigurationValue('title_message', $form_state->getValue('cr_email_signup_title_message'));
-    $this->setConfigurationValue('initial_message', $form_state->getValue('cr_email_signup_initial_message'));
-    $this->setConfigurationValue('first_success_message', $form_state->getValue('cr_email_signup_first_success_message'));
   }
 
 }
