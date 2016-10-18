05a96930b41161e638e688bd5c21639b698aea38
Zach Bimson <zach.bimson@gmail.com>
2016-10-18 17:03:40 +0100

    PLAT-510 Remove TI behat vars

--- profiles/cr/tests/drupal_ti/scripts/before_script.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_script.sh
@@ -3,10 +3,3 @@
 drush runserver --default-server=builtin 8080 &>/dev/null &
 phantomjs --webdriver=4444 > /dev/null &
 sleep 5
-# Simple script to install drupal for travis-ci running.
-# Clear caches and run a web server.
-#drupal_ti_clear_caches
-#drupal_ti_run_server
-# Start xvfb and selenium.
-#drupal_ti_ensure_xvfb
-#drupal_ti_ensure_webdriver
