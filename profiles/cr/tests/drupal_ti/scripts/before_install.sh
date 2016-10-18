08612aec894784b59ff1264948bb57023831ecb5
Zach Bimson <zach.bimson@gmail.com>
2016-10-18 11:19:57 +0100

    PLAT-510 Updates

--- profiles/cr/tests/drupal_ti/scripts/before_install.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_install.sh
@@ -1,6 +1,6 @@
 # Generate build.properties file on the fly
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
-printf 'db.querystring='$DRUPAL_TI_DB_URL >> build.properties
+printf 'db.querystring='$DB_URL >> build.properties
 # Output confirmation
 echo 'File: build.properties has been created.'
 # Remove gem cache
--- profiles/cr/tests/drupal_ti/scripts/before_script.sh
+++ profiles/cr/tests/drupal_ti/scripts/before_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Start webserver/ driver
+#drush runserver --default-server=builtin 8080 &>/dev/null &
+#phantomjs --webdriver=4444 > /dev/null &
+#sleep 5
 # Simple script to install drupal for travis-ci running.
 # Clear caches and run a web server.
 drupal_ti_clear_caches
