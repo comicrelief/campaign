d6b332c80926a9c37fcd2dbaf604d412659b20ab
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-10-31 16:24:49 +0000

    PLAT-528 fix play game when open new window

--- profiles/cr/modules/custom/cr_iframe/js/cr_iframe.js
+++ profiles/cr/modules/custom/cr_iframe/js/cr_iframe.js
@@ -7,11 +7,11 @@
     $('.iframe-block__play').click(function(event){
       if ($('html').hasClass('no-touchevents')) {
         event.preventDefault();
+        $(this).closest('.iframe-block__wrapper').addClass('iframe-block__wrapper--on');
+        var iframe = $(this).next(".iframe-block__game");
+        iframe.attr("src", iframe.data("src"));
+        $(this).find('.iframe-block__image').hide();
       }
-      $(this).closest('.iframe-block__wrapper').addClass('iframe-block__wrapper--on');
-      var iframe = $(this).next(".iframe-block__game");
-      iframe.attr("src", iframe.data("src"));
-      $(this).find('.iframe-block__image').hide();
     });
   });
 })(jQuery);
