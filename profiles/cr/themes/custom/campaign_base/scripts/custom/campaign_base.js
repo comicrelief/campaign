34f33fc3d1e37f46cdde25bc3e51888da50f2f2d
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-10-31 14:07:43 +0000

    RND-204 update js iframe

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -6,6 +6,11 @@
     // Activate lighcase
     // Video lightcase
     $('a[data-rel^=lightcase]').lightcase({
+      iframe: {
+        width: "100%",
+        height: "100%",
+        frameborder: 0
+      },
       onFinish : {
         custom: function() {
           var caption = $(this).parent().find('.media-block__caption');
