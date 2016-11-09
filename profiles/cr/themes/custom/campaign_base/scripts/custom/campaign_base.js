801f973289709a2e878396bb4f2d731abd4d815d
Gustavo Liedke <gustavoliedke@hotmail.com>
2016-11-09 22:40:32 +0000

    fix objectfit

--- profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
+++ profiles/cr/themes/custom/campaign_base/scripts/custom/campaign_base.js
@@ -1,6 +1,19 @@
 (function($, Drupal) {
-
   $( document ).ready(function() {
+    // IE fallback objectfit
+    if ( $('html').hasClass('no-objectfit') ) {
+      $('.objectfit').each(function () {
+        var $container = $(this),
+            imgUrl = $container.find('img').prop('srcset');
+        if (imgUrl) {
+          $container
+            .css('backgroundImage', 'url(' + imgUrl + ')')
+            .css('background-size', 'cover')
+            .addClass('compat-object-fit');
+          $container.find('img').hide();
+        }
+      });
+    }
     // Turn our boring select boxes into sexy jQuery UI selectboxes
     $('select').selectmenu({ style:'popup', width: '100%' });
     // Activate lighcase
@@ -32,9 +45,6 @@
     //   $("button.main-menu__icons-magnify").removeClass("active");
     //   $(".search-block").removeClass("show");
     // });
-	  
-	  newPosition();
-	  newHeight();
 
   });
     // reload our styling for select boxes after ajax
@@ -42,50 +52,4 @@
     $('select').selectmenu({ style:'popup', width: '100%' });
   });
 
-
-  sSize = $(window).width();
-
-      	
-  $(window).resize(function() {          
-    sSize = $(window).width();
-    if (sSize >= 740 || sSize == 1024) {
-      newPosition();
-      newHeight();
-    }
-  });
-
-  var newHeight = function() {
-    $('.cw-article__body--hide-show').each(function(){
-      var aHeight = $(this).height();
-      var hHeight = $(this).find('.cw-article__title').height();
-      var nHeight = aHeight - hHeight + 5;
-      $(this).parent().find('.cw-article__image').css('height',nHeight+'px');
-    });
-  }
-
-  var newPosition = function() {
-    $('.cw-article__body--hide-show').each(function(){
-      var aHeight = $(this).height();
-      var hHeight = $(this).find('.cw-article__title').height();
-      var topPos = aHeight - hHeight;
-      if (topPos > 245 && sSize < 1024) {
-        $(this).css('top','245px');
-      } else if (topPos > 229 && sSize < 1024) {
-        $(this).css('top','229px');
-      } else if (topPos > 335) {
-        $(this).css('top','316px');
-      } else {
-        $(this).css('top',topPos+'px');
-      }
-    });
-  }
-
-      
-  // Content wall body top position 
-  // Drupal.behaviors.contentWall = {
-  //   attach: function (context, settings) {
-	// 		$('.cw-article__body--hide-show').once('cw-hide-show', function () {
-	//      });   
-  //   }
-  // };
 })(jQuery, Drupal);
