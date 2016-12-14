81531daae81ad1447227ceb3d407d83f5eb1929a
Zach Bimson <zach.bimson@gmail.com>
2016-12-14 11:54:15 +0000

    PLAT-675 Initial attempt pushing to platform from travis

---
+++ profiles/cr/tests/scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Only continue if we are on the "develop" branch
+echo $TRAVIS_COMMIT
+if [[ $TRAVIS_BRANCH == *"develop"* ]]
+then
+  echo "Pushing to platform.sh develop env."
+  # Add platform.sh remote
+  git remote add platform git.eu.platform.sh:tx3mbsqmxtu74.git
+  git push platform develop --force
+fi
