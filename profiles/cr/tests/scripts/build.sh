9ed274d5c19349beeee5afdf9244ed46cc91746e
Zach Bimson <zach.bimson@gmail.com>
2016-12-14 16:30:08 +0000

    PLAT-675 Trial StrictHostKeyChecking

--- profiles/cr/tests/scripts/build.sh
+++ profiles/cr/tests/scripts/build.sh
@@ -3,6 +3,7 @@
 echo $TRAVIS_COMMIT
 if [[ $TRAVIS_BRANCH == *"feature/PLAT-675_platenv_develop"* ]]
 then
+
   # Decrypt travis platform.sh ssh key
   openssl aes-256-cbc -K $encrypted_7d6d79e53800_key -iv $encrypted_7d6d79e53800_iv -in travis_rsa.enc -out travis_rsa -d
   # Add platform.sh remote
@@ -10,6 +11,7 @@ then
   chmod 600 travis_rsa # this key should have push access
   ssh-add travis_rsa
   git remote add platform git.eu.platform.sh:tx3mbsqmxtu74.git
+  ssh -o "StrictHostKeyChecking no" *
   git push platform develop --force
   echo "Pushing to platform.sh develop env."
 fi
