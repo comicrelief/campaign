0171ae166de3dff954f76ca4af7bef4050fc6ea0
Zach Bimson <zach.bimson@gmail.com>
2016-12-14 15:31:47 +0000

    PLAT-675 Hopefully the correct dycrpt key now!

--- profiles/cr/tests/scripts/build.sh
+++ profiles/cr/tests/scripts/build.sh
@@ -4,7 +4,7 @@ echo $TRAVIS_COMMIT
 if [[ $TRAVIS_BRANCH == *"feature/PLAT-675_platenv_develop"* ]]
 then
   # Decrypt travis platform.sh ssh key
-  openssl aes-256-cbc -K $encrypted_0a6446eb3ae3_key -iv $encrypted_0a6446eb3ae3_key -in travis_rsa.enc -out travis_rsa -d
+  openssl aes-256-cbc -K $encrypted_7d6d79e53800_key -iv $encrypted_7d6d79e53800_iv -in travis_rsa.enc -out travis_rsa -d
   # Add platform.sh remote
   eval "$(ssh-agent -s)"
   chmod 600 travis_rsa # this key should have push access
