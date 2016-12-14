ce09bfae4b279bfbc8470cb6663eb1181fda54b2
Zach Bimson <zach.bimson@gmail.com>
2016-12-14 11:59:07 +0000

    PLAT-675 Temp branch based commit, will revert if working

--- profiles/cr/tests/scripts/build.sh
+++ profiles/cr/tests/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Only continue if we are on the "develop" branch
 echo $TRAVIS_COMMIT
-if [[ $TRAVIS_BRANCH == *"develop"* ]]
+if [[ $TRAVIS_BRANCH == *"feature/PLAT-675_platenv_develop"* ]]
 then
   echo "Pushing to platform.sh develop env."
   # Add platform.sh remote
