96f90d6b6f61ac0c6b210cf6d522f98acb58fd38
Peter Vanhee <peter.vanhee@gmail.com>
2016-12-21 17:27:45 +0000

    PLAT-336 Fix build file and refactor setting to deploy.craft

--- cr/tests/scripts/before-install.sh
+++ cr/tests/scripts/before-install.sh
@@ -3,7 +3,7 @@ set -e
 # Generate build.properties file on the fly
 printf 'drush.bin = ~/.composer/vendor/bin/drush.php\n' > build.properties
 printf 'db.querystring='$DB_URL'\n' >> build.properties
-printf 'deploy.with-craft=true' >> build.properties
+printf 'deploy.craft=true' >> build.properties
 # Output confirmation
 echo 'File: build.properties has been created.'
 # Get build dependencies
