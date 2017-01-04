701444fb026a91e65a38ad0232059ca91f6164e9
Heleen3258 <heleen3258@MACPRO-DEV02.local>
2017-01-04 10:14:21 +0000

    added vide library and custom script for video background to landing page module

---
+++ cr/modules/custom/cr_landing_page/js/cr_landing_page.js
@@ -0,0 +1,16 @@
+(function ($) {
+	$(document).ready(function () {
+		console.log("custom script");
+		//Background video using vide library
+		//get all video backgrounds
+		$( '.bg_video' ).each(function( index ) {
+		  var path = $(this).data('vide-bg');
+		  var options = $(this).data('vide-options');
+		  //trim spaces from path
+		  path = path.trim();
+		  console.log(path);
+		  // initialization
+		  $(this).vide(path, options);
+		});
+	});
+})(jQuery);
