5ce85ea9612dbabfbaec14abe05ad7a3295358e4
Heleen3258 <heleen3258@MACPRO-DEV02.local>
2017-01-06 16:19:38 +0000

    plat-672 set poster image to none, added script to show video only from desktop breakpoint on, centered video.

--- cr/modules/custom/cr_landing_page/js/cr_landing_page.js
+++ cr/modules/custom/cr_landing_page/js/cr_landing_page.js
@@ -1,15 +1,66 @@
 (function ($) {
-	$(document).ready(function () {
-		//Background video using vide library
-		//get all video backgrounds
-		$( '.bg_video' ).each(function( index ) {
-		  var path = $(this).data('vide-bg');
-		  var options = $(this).data('vide-options');
-		  //trim spaces from path
-		  path = path.trim();
-		  console.log(path);
-		  // initialization
-		  $(this).vide(path, options);
-		});
+
+	$(document).ready(function() {
+		if ($( '.bg_video' ).length) {
+
+	    var videoExists = false;
+	    var breakpoint = window.matchMedia("(min-width: 1024px)");
+	    var video;
+
+			// Instatiates background video using vide library
+			function backgroundVideo() {
+				var videoArray = [];
+				//get all video backgrounds 
+				$( '.bg_video' ).each(function(index){
+		      var path = $(this).data('vide-bg');
+		      var options = $(this).data('vide-options');
+		      //trim spaces from path
+					path = path.trim();
+					// initialization
+				  videoArray[index] = $(this).vide(path, options);
+				});
+				return videoArray;
+			}
+
+			function breakpointMatches(mql) {
+
+				if (mql.matches === true && !videoExists) {
+	          //instatiate video when resizing to LG
+	          video = backgroundVideo();
+	          video = video[0];
+	          videoExists = true;
+	          $( '.promo-header__bg-image' ).hide();
+	        }
+	        if (mql.matches === false && videoExists) {
+	          // remove video and show bg_image when resizing to SM again
+			  		$( '.promo-header__bg-image' ).show();
+			  		$( '.bg_video' ).hide();
+	        }
+	        else if(mql.matches === true && videoExists) {
+	        	// hide bg_image and show video when resizing to LG again
+				  	$( '.bg_video' ).show();
+			  		$( '.promo-header__bg-image' ).hide();
+				  }  
+			}
+
+	    // LG breakpoint on load
+			if( breakpoint.matches ){
+				// instatiate video and hide bg_image
+	      video = backgroundVideo();
+	      video = video[0];
+	      videoExists = true;
+
+	      $( '.promo-header__bg-image' ).hide();
+
+	      // add listener to hide video if we resize below 1024
+	      breakpoint.addListener(breakpointMatches);
+			} 
+
+	    // SM or MD breakpoint on load
+	    else {
+	      // add listener to instantiate/show video if we resize above 1024
+	      breakpoint.addListener(breakpointMatches);
+	    }
+  	}
 	});
 })(jQuery);
