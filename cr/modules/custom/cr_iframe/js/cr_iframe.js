0258c428a70e70029d2be290394dc004eda0b976
Gustavo Liedke <gustavoliedke@hotmail.com>
2017-01-18 15:12:45 -0200

    PLAT-708 show load mobile too

--- cr/modules/custom/cr_iframe/js/cr_iframe.js
+++ cr/modules/custom/cr_iframe/js/cr_iframe.js
@@ -4,11 +4,6 @@
 
 (function ($) {
   $(document).ready(function () {
-    if ($('.iframe-block').hasClass('iframe-block__load-now') && $('html').hasClass('no-touchevents')) {
-      var iframe_load = $(".iframe-block__game");
-      $(iframe_load).attr("src", iframe_load.data("src"));
-      $('.iframe-block__image').hide();
-    }
     $('.iframe-block__play').click(function(event){
       if ($('html').hasClass('no-touchevents')) {
         event.preventDefault();
