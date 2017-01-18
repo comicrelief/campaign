622ce216152b4874034bd518c33869e0c4cf34c8
Gustavo Liedke <gustavoliedke@hotmail.com>
2017-01-18 13:46:35 -0200

    PLAT-708 js update layout class style

--- cr/modules/custom/cr_iframe/js/cr_iframe.js
+++ cr/modules/custom/cr_iframe/js/cr_iframe.js
@@ -4,6 +4,11 @@
 
 (function ($) {
   $(document).ready(function () {
+    if ($('.iframe-block').hasClass('iframe-block__load-now') && $('html').hasClass('no-touchevents')) {
+      var iframe_load = $(".iframe-block__game");
+      $(iframe_load).attr("src", iframe_load.data("src"));
+      $('.iframe-block__image').hide();
+    }
     $('.iframe-block__play').click(function(event){
       if ($('html').hasClass('no-touchevents')) {
         event.preventDefault();
