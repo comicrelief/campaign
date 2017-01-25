a6e2f41c24bf89c47dfd2ea89455f0d08529a42e
Gustavo Liedke <gustavoliedke@hotmail.com>
2017-01-25 16:12:51 +0000

    PLAT-720 update cr nav js

--- cr/modules/custom/cr_navigation/js/cr_navigation.js
+++ cr/modules/custom/cr_navigation/js/cr_navigation.js
@@ -15,85 +15,42 @@
       });
     },
 
+    cloneNav: function (context, settings) {
+      var _base = Drupal.behaviors.crNavigation;
+      $( ".menu--main .menu-item:nth-child(1), .menu--main .menu-item:nth-child(2), .menu--main .menu-item:nth-child(3)" ).clone().appendTo( ".meta-nav .meta-nav__feature-items" );
+    },
+
+    lastclassNav: function (context, settings) {
+      var _base = Drupal.behaviors.crNavigation;
+      $( ".menu--main .menu-item:last-child" ).addClass( "menu-item__last" );
+    },
+
     setUpNav: function (context, settings) {
       var _base = Drupal.behaviors.crNavigation;
 
-      $('#main-menu .menu-item a, #block-kidsmenu > .menu .menu-item a').wrapInner('<span class="menu-item__text"></span>');
+      _base.cloneNav();
 
-      _base.duplicateParentLink();
+      _base.lastclassNav();
 
       _base.handleClick();
 
-      /* Setup the Smartmenus plugin with our main menu */
-      $('#main-menu, #block-kidsmenu > .menu').smartmenus({
-        subIndicatorsText: "",
-        keepHighlighted: false,
-        hideOnClick: true,
-      });
-
-      /* Bind the 'show' function to also hide all the other submenus */
-      $('#main-menu').bind('activate.smapi', function (e, menu) {
-        $('#main-menu').smartmenus('menuHideAll');
-      });
-
-      $('#block-kidsmenu > .menu').bind('activate.smapi', function (e, menu) {
-        $('#block-kidsmenu > .menu').smartmenus('menuHideAll');
-      });
-
-    },
-
-    /* Updates empty duplicate link (added by template) with the parent item's text and link, dynamically */
-    duplicateParentLink: function (context, settings) {
-
-      /* Update text and link */
-      $('.menu--main > .menu > .menu-item--expanded, .menu--kids-menu > .menu > .menu-item--expanded').each(function () {
-
-        $this = $(this);
-
-        // Populate duplicate link with parent link info.
-        $(this).children('ul.menu')
-          .find('.menu-item--duplicate a')
-            .attr("href", $this.children('a').attr('href'))
-              .find('span').text($this.children('a').text());
-      });
     },
 
     /* Click event handler trigger our toggle event */
     handleClick: function (context, settings) {
 
       var _base = Drupal.behaviors.crNavigation;
-      
-      $('button.main-menu-toggle').on('click', function (e) {
-        _base.toggleMenu();
-      });
 
       // Close any active navs when we're toggling on other buttons in the nav
-      $('.main-menu__icons button:not(.main-menu-toggle)').on('click', function (e) {
+      $('.meta-nav__icons button:not(.meta-nav-toggle)').on('click', function (e) {
         
         // Remove active class from hamburger nav to collapse it
-        $('button.main-menu-toggle.is-active').removeClass('is-active');
+        $('button.meta-nav-toggle.is-active').removeClass('is-active');
 
         // Remove active class from kids menu
         $('#main-menu, #block-kidsmenu > .menu.menu-open').removeClass('menu-open');
       });
     },
 
-    /* Update the main menu based on state, and hide other nav item dropdowns where appropriate */
-    toggleMenu: function (context, settings) {
-
-      $thisMenuButton = $('button.main-menu-toggle');
-      
-      // Change state for visual effect.
-      $thisMenuButton.toggleClass('is-active');
-
-      // Change state of menu itself.
-      $('#main-menu, #block-kidsmenu > .menu').toggleClass('menu-open');
-
-      // If we've just activated our main menu, remove all active/show states from other nav dropdowns
-      if ( $thisMenuButton.hasClass('is-active')) {
-        $('.main-menu__icons button:not(.main-menu-toggle)').removeClass('active');
-        $('#block-emailsignupblockhead, .search-overlay').removeClass('show');
-      }
-    },
   };
 })(jQuery);
