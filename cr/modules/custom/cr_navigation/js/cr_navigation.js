d194784a090cb20c48e1ff73bcecb3a9459d7a0f
Gustavo Liedke <gustavoliedke@hotmail.com>
2017-02-10 13:06:03 +0000

    RND-619 make more generic

--- cr/modules/custom/cr_navigation/js/cr_navigation.js
+++ cr/modules/custom/cr_navigation/js/cr_navigation.js
@@ -3,23 +3,20 @@
  */
 
 (function ($) {
-console.log('vaiaporra');
   Drupal.behaviors.crNavigation = {
 
     attach: function (context, settings) {
       var _base = Drupal.behaviors.crNavigation;
 
-      $('.menu--main, .menu--kids-menu').once('crNavigation').each(function () {
-        console.log('oioi');
-        $(this).addClass("crNavigation-processedona");
+      $('.header__inner-wrapper nav.navigation').once('crNavigation').each(function () {
+        $(this).addClass("crNavigation-processed");
           _base.setUpNav();
       });
     },
 
     cloneNav: function (context, settings) {
       var _base = Drupal.behaviors.crNavigation;
-      console.log('two');
-      $( ".menu--main .menu--level-0 .menu-item:nth-child(1), .menu--main .menu--level-0 .menu-item:nth-child(2), .menu--main .menu--level-0 .menu-item:nth-child(3)" ).clone().appendTo( ".feature-nav .feature-nav__items" );
+      $( ".header__inner-wrapper nav.navigation > .menu .menu-item:nth-child(1), .header__inner-wrapper nav.navigation > .menu .menu-item:nth-child(2), .header__inner-wrapper nav.navigation > .menu .menu-item:nth-child(3)" ).clone().appendTo( ".feature-nav .feature-nav__items" );
     },
 
     /* Click event handler trigger our toggle event */
@@ -33,14 +30,13 @@ console.log('vaiaporra');
         // Remove active class from hamburger nav to collapse it
         $('button.feature-nav-toggle.is-active').removeClass('is-active');
 
-        // Remove active class from kids menu
-        $('#main-menu, #block-kidsmenu > .menu.menu-open').removeClass('menu-open');
+        // Remove active class from kids menu | move it to rnd17
+        $('#block-kidsmenu > .menu.menu-open').removeClass('menu-open');
       });
     },
 
     setUpNav: function (context, settings) {
       var _base = Drupal.behaviors.crNavigation;
-      console.log('one');
       _base.cloneNav();
 
       _base.handleClick();
