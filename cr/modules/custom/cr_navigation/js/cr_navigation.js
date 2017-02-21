6a32e40c9fad5caf6706cc244424d888b8e87d3c
Andy Phipps <andyp@MacAndMe.local>
2017-02-21 17:46:00 +0000

    PLAT-762: basic JS functionality

--- cr/modules/custom/cr_navigation/js/cr_navigation.js
+++ cr/modules/custom/cr_navigation/js/cr_navigation.js
@@ -25,6 +25,10 @@
     /* Click event handler trigger our toggle event */
     handleClick: function (context, settings) {
 
+      var navClasses = ['meta-icons__esu-toggle', 'feature-nav-toggle,', 'block--cr-email-signup--step-1'];
+
+      console.log("nav items", navClasses);
+
       var _base = Drupal.behaviors.crNavigation;
 
       // Close any active navs when we're toggling on other buttons in the nav
@@ -34,14 +38,29 @@
         $('button.feature-nav-toggle.is-active').removeClass('is-active');
 
       });
+
+      // Close all overlays and dropdowns when we're clicking on other content
+      $(document).on('click', function (e) {
+        
+        // Only trigger the clos
+        if (!$(e.target).is('.meta-icons__esu-toggle, .feature-nav-toggle, .meta-icons__magnify')) {
+          console.log("not the nav icons, do stuff");
+
+          $('button.feature-nav-toggle.is-active').removeClass('is-active');
+          $('#block-campaign-base-main-menu.show, .search-overlay.show, .block--cr-email-signup--head').removeClass('show');
+          $('.meta-icons__esu-toggle.active, meta-icons__magnify.active').removeClass('active');
+
+        } else {
+          console.log("is one of the nav icons, dont stuff");
+
+        }
+      });
     },
 
     setUpNav: function (context, settings) {
       var _base = Drupal.behaviors.crNavigation;
       _base.cloneNav();
-
       _base.handleClick();
-
     },
 
   };
