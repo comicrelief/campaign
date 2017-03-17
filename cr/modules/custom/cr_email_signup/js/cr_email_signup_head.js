c00c4b9bc8744db3977b704daaa0dd506967256d
Andy Phipps <andyp@MacAndMe.local>
2017-03-17 17:17:35 +0000

    PLAT-663: Automatically focus on ESU textfield upon opening

--- cr/modules/custom/cr_email_signup/js/cr_email_signup_head.js
+++ cr/modules/custom/cr_email_signup/js/cr_email_signup_head.js
@@ -4,10 +4,13 @@
 
 (function ($) {
   $(document).ready(function () {
+    
     $("button.meta-icons__esu-toggle").on("click", function() {
       $(this).toggleClass("active");
-      $(".block--cr-email-signup--head").toggleClass("show");
+      $(".block--cr-email-signup--head").toggleClass("show")
+        .find('#edit-email').focus();
     });
+
     $(".block--cr-email-signup--head .icon").on("click", function() {
       $("button.meta-icons__esu-toggle").removeClass("active");
       $(".block--cr-email-signup--head").removeClass("show");
