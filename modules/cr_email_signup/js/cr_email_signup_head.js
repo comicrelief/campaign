0589e5165faa97ca970af09eb592b5fbd52e07d6
Heleen3258 <heleen3258@MACPRO-DEV02.local>
2017-04-05 14:46:38 +0100

    PLAT-663 fixed keyboardpress to submit form

--- modules/cr_email_signup/js/cr_email_signup_head.js
+++ modules/cr_email_signup/js/cr_email_signup_head.js
@@ -16,12 +16,12 @@
       $(".block--cr-email-signup--head").removeClass("show");
     });
 
-    // Handler for keypresses
-    $("form#cr-header-email-signup-form").keypress(function(event) {
+    // Handler for keypresses for all esu forms
+    $(".form-submit").keypress(function(event) {
       // If the key pressed has the right keycode
       if (event.which == 13) {
         // Submit form with fake mouse event; jQuery form submit function doesn't work here!
-        $('#edit-step1').mousedown();
+        $(event.target).mousedown();
       }
     });
   });
