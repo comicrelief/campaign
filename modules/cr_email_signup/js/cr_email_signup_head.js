dd70c2be6e8e71aa9e2fbeb83f165e42d594a033
Heleen3258 <heleen3258@MACPRO-DEV02.local>
2017-04-07 16:07:27 +0100

    PLAT-663 added focus back on input fields in case of error or select menu and added possibility to submit form by pressing enter in input field

--- modules/cr_email_signup/js/cr_email_signup_head.js
+++ modules/cr_email_signup/js/cr_email_signup_head.js
@@ -16,24 +16,52 @@
       $(".block--cr-email-signup--head").removeClass("show");
     });
 
-    // Handler for keypresses for all esu forms
+    // Handler to submit form by pressing enter on button for all esu forms
     $(".form-submit").keypress(function(event) {
       // If the key pressed has the right keycode
       if (event.which == 13) {
         // Submit form with fake mouse event; jQuery form submit function doesn't work here!
         $(event.target).mousedown();
+        refocus();
+      }
+    });
+    // Handler to sumbmit form by pressing enter in input field
+    $(".form-text").keypress(function(event) {
+      if (event.which == 13) {
+        var parent = $(event.target).parent();
+        // Different layer in case of standard ESU form
+        if ( $(parent).parent().attr('id') == 'cr-email-signup-form' ) {
+          // Submit this way only for step 1 in form. (Step 2 requires tabbing to button)
+          var submit = $(parent).parent().find(".step1");
+        }
+        else {
+          var submit = $(parent).next(".form-submit");
+        }
+        $(submit).mousedown();
+        refocus();
+      }
+    });
 
-        // set focus back to input when error - create function for this!
-        $(document).ajaxComplete(function() {
-          var blocks = $(".block-cr-email-signup");
-          for (i=0; i<blocks.length; i++) {
-            var block = blocks[i];
-            if ($(block).hasClass("block--cr-email-signup--error")) {
+    function refocus() {
+      // Set focus back to input or select menu
+      $(document).ajaxComplete(function() {
+        var blocks = $(".block-cr-email-signup");
+        for ( i=0 ; i<blocks.length; i++ ) {
+          var block = blocks[i];
+          if ( $(block).hasClass("block--cr-email-signup--error") ) {
+            if ( $(block).hasClass("error--firstname") ) {
+              $("#edit-firstname").focus();
+            }
+            else {
               $(block).find(".form-text").focus();
             }
           }
-        });
-      }
-    });  
+          if ( $(block).hasClass("block--cr-email-signup--step-2") ) {
+            $(block).find(".ui-selectmenu-button").focus();
+          }
+        }
+      });
+    }
+    
   });
 })(jQuery);
