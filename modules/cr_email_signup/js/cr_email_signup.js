9c172804b64b2d6f3d974f076adb896c08efef85
Heleen3258 <heleen3258@MACPRO-DEV02.local>
2017-04-19 17:36:53 +0100

    PLAT-663 refactored ESU js

--- modules/cr_email_signup/js/cr_email_signup.js
+++ modules/cr_email_signup/js/cr_email_signup.js
@@ -12,6 +12,7 @@
     hiddenDeviceFieldClass: '.esu-device',
     hiddenSourceFieldClass: '.esu-source',
     sourceValue: 'Header',
+    firstLoad: false,
    },
 
     attach: function (context, settings) {
@@ -19,9 +20,14 @@
       var _base = Drupal.behaviors.crEmailSignUp;
       var _settings = _base.settings;
 
-      $(_settings.genericEsuClass).once('crEMailSignup').each(function () {
-        $(this).addClass("crEMailSignup-processed");
-        _base.setDevice(this);
+      $(_settings.genericEsuClass).once('crEMailSignup').each(function() {
+
+        if(_settings.firstLoad == false) {
+          _base.setDevice(this);
+          _base.keyboardSubmit(this);
+          Drupal.behaviors.crEmailSignUp.settings.firstLoad = true;
+          _settings.firstLoad = Drupal.behaviors.crEmailSignUp.settings.firstLoad;
+        }
       });
     },
 
@@ -56,6 +62,58 @@
 
       // Use this value to set the hidden source field.
       $(context).find(_settings.hiddenSourceFieldClass).val(_settings.sourceValue);
+    },
+
+    keyboardSubmit: function (context, settings) {
+
+      var _base = Drupal.behaviors.crEmailSignUp;
+      var _settings = _base.settings;
+
+      // Handler to submit form by pressing enter on button for all esu forms
+      $(".form-submit, .form-text").keypress(function(event) {
+        $eventTarget = $(event.target);
+        // If the key pressed has the right keycode
+        if (event.which == 13) {
+          // Submit form with fake mouse event; jQuery form submit function doesn't work here!
+          if($(this).hasClass('form-submit')) {
+            $eventTarget.mousedown();
+          }
+          else {
+            $parent = $eventTarget.parent('.form-item');
+            $form = $parent.parent('form');
+            $submit = null; 
+            // Different layer in case of standard ESU form
+            if ( $form.attr('id') == 'cr-email-signup-form' ) {
+              // Submit this way only for step 1 in form. (Step 2 requires tabbing to button)
+              $submit = $form.find(".step1");
+            }
+            else {
+             $submit = $parent.next(".form-submit");
+            }
+            $submit.mousedown();
+          }
+          refocus();
+        }
+      });
+
+      function refocus() {
+        // Set focus back to input or select menu
+        $(document).ajaxComplete(function() {
+          if ( $(".block--cr-email-signup--error") ) {
+            $block = $(".block--cr-email-signup--error");
+            if ( $block.hasClass("error--firstname") ) {
+              $("#edit-firstname").focus();
+            }
+            else {
+              $block.find(".form-text").focus();
+            }
+          }
+          if ( $(".block--cr-email-signup--step-2") ) {
+            $block = $(".block--cr-email-signup--step-2");
+            $block.find("select, .ui-selectmenu-button").focus();
+          }
+        });
+      }
     },
   };
 })(jQuery);
--- modules/cr_email_signup/js/cr_email_signup_head.js
+++ modules/cr_email_signup/js/cr_email_signup_head.js
@@ -14,52 +14,6 @@
     $(".block--cr-email-signup--head .icon").on("click", function() {
       $("button.meta-icons__esu-toggle").removeClass("active");
       $(".block--cr-email-signup--head").removeClass("show");
-    });
-
-    // Handler to submit form by pressing enter on button for all esu forms
-    $(".form-submit").keypress(function(event) {
-      // If the key pressed has the right keycode
-      if (event.which == 13) {
-        // Submit form with fake mouse event; jQuery form submit function doesn't work here!
-        $(event.target).mousedown();
-        refocus();
-      }
-    });
-    // Handler to sumbmit form by pressing enter in input field
-    $(".form-text").keypress(function(event) {
-      if (event.which == 13) {
-        var parent = $(event.target).parent();
-        // Different layer in case of standard ESU form
-        if ( $(parent).parent().attr('id') == 'cr-email-signup-form' ) {
-          // Submit this way only for step 1 in form. (Step 2 requires tabbing to button)
-          var submit = $(parent).parent().find(".step1");
-        }
-        else {
-          var submit = $(parent).next(".form-submit");
-        }
-        $(submit).mousedown();
-        refocus();
-      }
-    });
-
-    function refocus() {
-      // Set focus back to input or select menu
-      $(document).ajaxComplete(function() {
-        if ( $(".block--cr-email-signup--error") ) {
-          var block = $(".block--cr-email-signup--error");
-          if ( $(block).hasClass("error--firstname") ) {
-            $("#edit-firstname").focus();
-          }
-          else {
-            $(block).find(".form-text").focus();
-          }
-        }
-        if ( $(".block--cr-email-signup--step-2") ) {
-          var block = $(".block--cr-email-signup--step-2");
-          $(block).find(".ui-selectmenu-button").focus();
-        }
-      });
-    }
-    
+    });    
   });
 })(jQuery);
