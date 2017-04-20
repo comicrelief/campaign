1325777084e0ead421b6cb0c5864c46fad6e8d62
Heleen3258 <heleen3258@MACPRO-DEV02.local>
2017-04-20 17:58:08 +0100

    PLAT-663 more refactoring

--- modules/cr_email_signup/js/cr_email_signup.js
+++ modules/cr_email_signup/js/cr_email_signup.js
@@ -14,6 +14,8 @@
     deviceValue: '',
     sourceValue: '',
     firstLoad: false,
+    formStep: null,
+    isFirstAjaxCall: true,
    },
 
     attach: function (context, settings) {
@@ -25,7 +27,7 @@
 
         if(_settings.firstLoad == false) {
           _base.setDevice(this);
-          _base.keyboardSubmit(context);
+          _base.keyboardSubmit();
           Drupal.behaviors.crEmailSignUp.settings.firstLoad = true;
           _settings.firstLoad = Drupal.behaviors.crEmailSignUp.settings.firstLoad;
         }
@@ -68,43 +70,64 @@
       $(context).find(_settings.hiddenDeviceFieldClass).val(_settings.deviceValue);
     },
 
-    keyboardSubmit: function (context, settings) {
+    keyboardSubmit: function () {
       
       var _base = Drupal.behaviors.crEmailSignUp;
       var _settings = _base.settings;
 
-      // Handler to submit form by pressing enter on button for all esu forms
-      $(".form-submit, .form-text", _settings.genericEsuClass).keypress(function(event) {
+      // Handler to submit form by pressing enter for all esu forms
+      $(".form-submit, .form-text, select, .ui-selectmenu-button", _settings.genericEsuClass).on('keypress', function(event) {
+        
         $eventTarget = $(event.target);
+        
         // If the key pressed has the right keycode
         if (event.which == 13) {
-          // Submit form with fake mouse event; jQuery form submit function doesn't work here!
+          // Reset flag for Ajax to run code again
+          Drupal.behaviors.crEmailSignUp.settings.isFirstAjaxCall = true;
+          
+          // Submit when focused on submit button
           if($(this).hasClass('form-submit')) {
+            // Submit form with fake mouse event; jQuery form submit function doesn't work here.
             $eventTarget.mousedown();
           }
+
+          // Submit when focused on the input field
           else {
             $parent = $eventTarget.parent('.form-item');
             $form = $parent.parent('form');
             $submit = null; 
-            // Different layer in case of standard ESU form
+            
+            // Submit button in different layer in standard ESU form
             if ( $form.attr('id') == 'cr-email-signup-form' ) {
               // Submit this way only for step 1 in form. (Step 2 requires tabbing to button)
               $submit = $form.find(".step1");
             }
+
+            // In all other forms the submit button is a next sibling 
             else {
-             $submit = $parent.next(".form-submit");
+              $submit = $parent.next(".form-submit");
             }
-            $submit.mousedown();
+            
+           $submit.mousedown();
           }
-          refocus();
         }
       });
 
-      function refocus() {
-        // Set focus back to input or select menu
-        $(document).ajaxComplete(function() {
-          if ( $(".block--cr-email-signup--error") ) {
+        
+      $(document).ajaxComplete(function(event) {
+        // Set focus back to input or select menu in case of an error
+        // Only run code once
+        if (Drupal.behaviors.crEmailSignUp.settings.isFirstAjaxCall ) {
+
+          if ( $(".block--cr-email-signup--step-2").length > 0 ) {
+            $block = $(".block--cr-email-signup--step-2");
+            // focus on select menu or jquery ui select menu
+            $block.find("select, .ui-selectmenu-button").focus();
+          }
+
+          if ( $(".block--cr-email-signup--error").length > 0 ) {
             $block = $(".block--cr-email-signup--error");
+
             if ( $block.hasClass("error--firstname") ) {
               $("#edit-firstname").focus();
             }
@@ -112,12 +135,15 @@
               $block.find(".form-text").focus();
             }
           }
-          if ( $(".block--cr-email-signup--step-2") ) {
-            $block = $(".block--cr-email-signup--step-2");
-            $block.find("select, .ui-selectmenu-button").focus();
-          }
-        });
-      }
+          // remove keypress event handler and re-attach it
+          $(".form-submit, .form-text, select, .ui-selectmenu-button", _settings.genericEsuClass).off('keypress');
+          Drupal.behaviors.crEmailSignUp.keyboardSubmit();
+        }
+
+        // Prevent Ajax from running code multiple times
+        Drupal.behaviors.crEmailSignUp.settings.isFirstAjaxCall = false;
+        
+      });
     },
   };
 })(jQuery);
