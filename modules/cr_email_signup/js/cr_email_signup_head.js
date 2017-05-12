6d4495d37b1dbf099df892b72d582eef016d6c0c
Gustavo Liedke <gustavoliedke@hotmail.com>
2017-05-12 16:50:25 +0100

    CR-9 update icons with links role btn

--- modules/cr_email_signup/js/cr_email_signup_head.js
+++ modules/cr_email_signup/js/cr_email_signup_head.js
@@ -4,15 +4,15 @@
 
 (function ($) {
   $(document).ready(function () {
-    
-    $("button.meta-icons__esu-toggle").on("click", function() {
+    $(".meta-icons__esu-toggle").on("click", function(event) {
+      event.preventDefault();
       $(this).toggleClass("active");
       $(".block--cr-email-signup--head").toggleClass("show")
         .find('#edit-email').focus();
     });
 
     $(".block--cr-email-signup--head .esu-head-close").on("click", function() {
-      $("button.meta-icons__esu-toggle").removeClass("active");
+      $(".meta-icons__esu-toggle").removeClass("active");
       $(".block--cr-email-signup--head").removeClass("show");
     });    
   });
