a08e740a5eef9bab37fa3833bd26de7a26c7ef60
Carlos Jimenez <Saphyel@gmail.com>
2017-10-17 15:30:24 +0100

    2.1.1 (#1270)
    * Update fastly (#1261)
    
    * Remove Optimizely (#1263)
    
    * 1262 - Remove optimizely
    
    * 1262 - Remove hook update
    
    * Update behat-tools (#1264)
    
    * Update drupal settings (#1269)
    
    * Update drupal settings
    
    * Update drupal settings for plat.sh
    
    * Update composer
    
    * Remove unused shared-folder
    
    * CTA styles update in content wall button dropdown (#1267)
    
    * 1141: added links to content wall teaser block button drop down and removed btn class from template
    
    * 1141: updated btn with cta in teaser block template
    
    * 1141: updated labels for button dropdown to cta styles in teaser block
    
    * Update ColourService.php

--- ci/platform/settings.php
+++ ci/platform/settings.php
@@ -1,24 +1,17 @@
 <?php
-/**
- * @file
- * Platform.sh example settings.php file for Drupal 8.
- */
 
-// Default Drupal 8 settings.
-//
-// These are already explained with detailed comments in Drupal's
-// default.settings.php file.
-//
-// See https://api.drupal.org/api/drupal/sites!default!default.settings.php/8
 $databases = [];
-$config_directories[CONFIG_SYNC_DIRECTORY] = 'sites/default/config';
-$settings['update_free_access'] = false;
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
+$config_directories = [];
+$settings['install_profile'] = 'cr';
+$settings['update_free_access'] = FALSE;
+$settings['skip_permissions_hardening'] = TRUE;
 $settings['file_scan_ignore_directories'] = [
-    'node_modules',
-    'bower_components',
+  'node_modules',
+  'bower_components',
 ];
 $settings['entity_update_batch_size'] = 50;
+$config_directories[CONFIG_SYNC_DIRECTORY] = 'sites/default/config';
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 // The hash_salt should be a unique random value for each application.
 // If left unset, the settings.platformsh.php file will attempt to provide one.
@@ -31,7 +24,6 @@ $settings['hash_salt'] = '7BbC-OM6nFz5BDRB9ksza__3PmJSrcZ-eHY1InKGUlt2cnXFsI6eAJ
 // Set up a config sync directory.
 //
 // This is defined inside the read-only "config" directory, deployed via Git.
-$settings['install_profile'] = 'cr';
 $settings['twig_tweak_enable_php_filter'] = true;
 
 $databases['default']['default'] = array (
