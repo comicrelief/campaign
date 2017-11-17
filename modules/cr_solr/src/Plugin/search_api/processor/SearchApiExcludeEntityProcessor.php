f00e073478b805780c82365d61e2a9c9860ee1ee
Saphyel <Saphyel@gmail.com>
2017-11-17 16:24:37 +0000

    [#1287] - Unset mp4 files

--- modules/cr_solr/src/Plugin/search_api/processor/SearchApiExcludeEntityProcessor.php
+++ modules/cr_solr/src/Plugin/search_api/processor/SearchApiExcludeEntityProcessor.php
@@ -4,6 +4,7 @@ namespace Drupal\cr_solr\Plugin\search_api\processor;
 
 use Drupal\media_entity\MediaInterface;
 use Drupal\search_api\Processor\ProcessorPluginBase;
+use Drupal\file\Entity\File;
 
 /**
  * Excludes media entities from node indexes.
@@ -26,6 +27,13 @@ class SearchApiExcludeEntityProcessor extends ProcessorPluginBase {
     foreach ($items as $item_id => $item) {
       $object = $item->getOriginalObject()->getValue();
       if ($object instanceof MediaInterface) {
+        if ($object->toArray()['bundle'][0]['target_id'] === 'cr_file') {
+          //load file id
+          $filename = File::load($object->toArray()['field_cr_file'][0]['target_id'])->getFilengetMimeTypeame();
+          if ($filename === 'video/mp4') {
+            unset($items[$item_id]);
+          }
+        }
       }
     }
   }
