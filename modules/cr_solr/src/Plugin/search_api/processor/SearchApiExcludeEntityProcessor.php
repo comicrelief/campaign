fa6a726bd28cc8393f21f1dacc6cfc3284c32171
Saphyel <Saphyel@gmail.com>
2017-11-20 11:50:20 +0000

    Fix search in develop

--- modules/cr_solr/src/Plugin/search_api/processor/SearchApiExcludeEntityProcessor.php
+++ modules/cr_solr/src/Plugin/search_api/processor/SearchApiExcludeEntityProcessor.php
@@ -3,6 +3,7 @@
 namespace Drupal\cr_solr\Plugin\search_api\processor;
 
 use Drupal\media_entity\MediaInterface;
+use Drupal\search_api\IndexInterface;
 use Drupal\search_api\Processor\ProcessorPluginBase;
 use Drupal\file\Entity\File;
 
@@ -14,22 +15,38 @@ use Drupal\file\Entity\File;
  *   label = @Translation("Ignore background media"),
  *   description = @Translation("Don't index mp4 files."),
  *   stages = {
- *     "preprocess_index" = -50
+ *     "alter_items" = 0
  *   }
  * )
  */
 class SearchApiExcludeEntityProcessor extends ProcessorPluginBase {
 
+
   /**
    * {@inheritdoc}
    */
-  public function preprocessIndexItems(array $items) {
+  public static function supportsIndex(IndexInterface $index) {
+    foreach ($index->getDatasources() as $datasource) {
+      if ($datasource->getEntityTypeId() === 'media') {
+        return TRUE;
+      }
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alterIndexedItems(array &$items) {
+    /** @var \Drupal\search_api\Item\ItemInterface $item */
     foreach ($items as $item_id => $item) {
       $object = $item->getOriginalObject()->getValue();
       if ($object instanceof MediaInterface) {
         if ($object->toArray()['bundle'][0]['target_id'] === 'cr_file') {
+          $fid = $object->toArray()['field_cr_file'][0]['target_id'];
           //load file id
-          $filename = File::load($object->toArray()['field_cr_file'][0]['target_id'])->getFilengetMimeTypeame();
+          $filename = File::load($fid)->getMimeType();
           if ($filename === 'video/mp4') {
             unset($items[$item_id]);
           }
