f689316253603c064cebdfa0c011d06ca5b13e16
Andy E Phipps <a.phipps@comicrelief.com>
2018-02-08 17:39:56 +0000

    Update button text to better describe to screenreaders

--- modules/cr_email_signup/src/Form/HeadEsu.php
+++ modules/cr_email_signup/src/Form/HeadEsu.php
@@ -14,4 +14,20 @@ class HeadEsu extends FundraiseSignUp {
     return 'cr_header_email_signup_form';
   }
 
+   /**
+   * {@inheritdoc}
+   */
+  protected function esuSubmitFields() {
+    $form[$this->getFormId() . '_step1'] = [
+      '#type' => 'button',
+      '#name' => 'step1',
+      '#value' => $this->t('Submit'),
+      '#attributes' => ['class' => ['step1']],
+      '#ajax' => [
+        'callback' => [$this, 'processSteps'],
+      ],
+    ];
+    return $form;
+  }
+
 }
