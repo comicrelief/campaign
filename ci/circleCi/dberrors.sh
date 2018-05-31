344e4ace9287ef38e5b54f4c87e999c8ede6cb28
Adam Clark <a.clark@comicrelief.com>
2018-05-31 16:16:08 +0100

    Fix db errors from circleci

--- ci/circleCi/dberrors.sh
+++ ci/circleCi/dberrors.sh
@@ -3,11 +3,11 @@ set -e
 # Simple script to check code quality.
 cd web
 touch tmp.txt
-drush wd-show --severity=0 > tmp.txt
-drush wd-show --severity=1 > tmp.txt
-drush wd-show --severity=2 > tmp.txt
-drush wd-show --severity=3 > tmp.txt
-drush wd-show --severity=4 > tmp.txt
+drush wd-show --severity=Emergency > tmp.txt
+drush wd-show --severity=Alert > tmp.txt
+drush wd-show --severity=Critical > tmp.txt
+drush wd-show --severity=Error > tmp.txt
+drush wd-show --severity=Warning > tmp.txt
 
 FILESIZE=$(cat tmp.txt | wc -c)
 
